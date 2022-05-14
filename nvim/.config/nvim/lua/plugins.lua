6b6b400be47fcf7ca271cac0cce6709856563ef6
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-05-13 23:05:45 -0400

    add gitignore

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -31,6 +31,7 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
+  use 'wbthomason/packer.nvim' -- Have packer manage itself
   use {
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
