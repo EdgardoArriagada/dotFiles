88657bd13b6e9947bae0d91211851cf5ff37168b
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-05-14 11:33:12 -0400

    make init.lua more explicit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,10 @@
-require('plugins')
-require('mappings')
+-- References to ./lua/
+-- Plugin management via Packer
+require("plugins")
+-- Vim mappings, see lua/config/which.lua for more mappings
+require("mappings")
+-- All non plugin related (vim) options
+require("options")
+-- Vim autocommands/autogroups
+require("autocmd")
 
