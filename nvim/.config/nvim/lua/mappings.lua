a7d0df25d0228325f3a991513725542df4004ab4
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-05-14 11:35:18 -0400

    move options to its own file

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -1,38 +1,3 @@
-local o = vim.o
-local wo = vim.wo
-local bo = vim.bo
-
--- map the leader key
-vim.api.nvim_set_keymap('n', '<Space>', '', {})
-vim.g.mapleader = ' '  -- 'vim.g' sets global variables
-
-vim.o.clipboard = "unnamedplus"
-
--- TODO: paste without yanking
--- vnoremap('ap' "'$hpgv\"'.v:register.'y`>'")
--- xnoremap <expr> p 'pgv"'.v:register.'y`>'
--- xnoremap <expr> P 'Pgv"'.v:register.'y`>'
-
--- global options
-o.swapfile = true
-o.dir = '/tmp'
-o.smartcase = true
-o.laststatus = 2
-o.hlsearch = true
-o.incsearch = true
-o.ignorecase = true
-o.scrolloff = 12
-vim.cmd([[colorscheme gruvbox]])
--- ... snip ...
-
--- window-local options
-wo.number = true
-wo.relativenumber = true
-wo.wrap = false
-
--- buffer-local options
-bo.expandtab = true
-
 local function map(mode, lhs, rhs, opts)
   local options = { noremap = true }
   if opts then
