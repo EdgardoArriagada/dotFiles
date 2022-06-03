37b989919cf130801c6532a64ac6d7076c8171c1
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-06-03 16:38:51 -0400

    expand plugin helpers

--- nvim/.config/nvim/lua/utils/lineGetters.lua
+++ nvim/.config/nvim/lua/utils/lineGetters.lua
@@ -2,9 +2,10 @@ local keymap = vim.keymap
 local line = vim.fn.line
 local indent = vim.fn.indent
 
-keymap.set('n', "<bs>", function()
-  getSameIndentLine('j')
-end, { noremap = true, silent = true })
+-- to debug
+--keymap.set('n', "<bs>", function()
+--  local lol = getSameIndentLine('j')
+--end, { noremap = true, silent = true })
 
 local line = vim.fn.line
 
@@ -25,7 +26,60 @@ function getSameIndentLine(direction)
   while not existsSameIndent and lineMarker ~= endOfFile do
     lineMarker = lineMarker + inc
     existsSameIndent = indent(lineMarker) == originalIndent
-    -- We stap here
 
+    if existsSameIndent and isEmptyLine(lineMarker) then
+      existsSameIndent = false
+    end
+  end
+
+  if existsSameIndent then
+    return lineMarker
+  else
+    return line('.')
   end
 end
+
+function getLastMatchingIndent(direction)
+  local inc, endOfFile = getProps(direction)
+
+  local lineMarker = line('.')
+  local originalIndent = indent('.')
+
+  while lineMarker ~= endOfFile do
+    lineMarker = lineMarker + inc
+    existsSameIndent = indent(lineMarker) == originalIndent
+
+    if not (existsSameIndent or isEmptyLine(lineMarker)) then
+      break
+    end
+  end
+
+  return lineMarker
+end
+
+-- func! GetStopLine(direction)
+  -- let [inc, endOfFile] = s:getProps(a:direction)
+
+  -- let lineMarker = line('.')
+  -- let s:isStartEmpty = IsEmptyLine(lineMarker)
+  -- let s:originalIndent = indent('.')
+
+  -- func! s:didSwitch(line)
+    -- if s:isStartEmpty == 1
+      -- return !IsEmptyLine(a:line)
+    -- else
+      -- return IsEmptyLine(a:line)
+    -- endif
+  -- endfunc
+
+  -- while !s:didSwitch(lineMarker) && lineMarker != endOfFile
+    -- let lineMarker += inc
+
+    -- if !IsEmptyLine(lineMarker) && indent(lineMarker) < s:originalIndent
+      -- let lineMarker -= inc
+      -- break
+    -- endif
+  -- endwhile
+
+  -- return lineMarker
+-- endfunc
