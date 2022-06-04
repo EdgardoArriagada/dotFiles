8aefacbfd8c3d9c271a194ee01e7078a71de678e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-06-03 21:00:21 -0400

    translate perfectlySelectString

--- nvim/.config/nvim/lua/customPluggins/init.lua
+++ nvim/.config/nvim/lua/customPluggins/init.lua
@@ -1,3 +1,4 @@
 require('customPluggins.goLessDeeperIndent')
 require('customPluggins.powerSelection')
 require('customPluggins.vaqueroSelect')
+require('customPluggins.perfectlySelectString')
---
+++ nvim/.config/nvim/lua/customPluggins/perfectlySelectString.lua
@@ -0,0 +1,74 @@
+keymap.set({ 'o', 'v' }, "'", function()
+  execute('normal <Esc>')
+  perfectlySelectString({"'", '"'})
+end, { noremap = true, silent = true })
+
+keymap.set({ 'o', 'v' }, '"', function()
+  execute('normal <Esc>')
+  perfectlySelectString({'"', "'"})
+end, { noremap = true, silent = true })
+
+local function didSelectBetween(middle)
+  local left = col('v')
+  local right = col('.')
+  return left ~= right and left -1 <= middle and middle <= right +1
+end
+
+local function restorePos(position)
+  setpos('.', position)
+end
+
+local function pressEscape()
+  execute('normal <Esc>')
+end
+
+local function didSelect()
+  return col('.') ~= col('v')
+end
+
+function perfectlySelectString(quotes)
+  local savedPos = getpos('.')
+  local savedColumn = savedPos[3]
+
+  local function restoreValues()
+    restorePos(savedPos)
+    pressEscape()
+  end
+
+  local function didMove()
+    return col('.') ~= savedColumn
+  end
+
+  -- If did select between cursor
+  for i, quote in ipairs(quotes) do
+    execute('normal vi'..quote)
+
+    if didSelectBetween(savedColumn) then
+      return
+    end
+
+    restoreValues()
+  end
+
+  for i, quote in ipairs(quotes) do
+    -- If did select goind forward
+    execute('normal vi'..quote)
+
+    if didSelect() or didMove() then
+      return
+    end
+
+    restoreValues()
+  end
+
+  for i, quote in ipairs(quotes) do
+    -- If did select goind backwards
+    execute('normal F'..quote..'vi'..quote)
+
+    if didSelect() or didMove() then
+      return
+    end
+
+    restoreValues()
+  end
+end
