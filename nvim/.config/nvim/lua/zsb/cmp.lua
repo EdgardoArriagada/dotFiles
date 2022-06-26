eceeb894c457d4d9d37d568e7209188ebce36923
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-06-26 15:08:09 -0400

    activate nice icons for completions

--- nvim/.config/nvim/lua/zsb/cmp.lua
+++ nvim/.config/nvim/lua/zsb/cmp.lua
@@ -99,7 +99,7 @@ cmp.setup {
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
       -- Kind icons
-      -- vim_item.kind = string.format("%s", kind_icons[vim_item.kind]) not working on crappy macos
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
@@ -123,5 +123,10 @@ cmp.setup {
   experimental = {
     ghost_text = false,
     native_menu = false,
+  },
+  window = {
+    documentation = {
+      border = {'╔', '═', '╗', '║', '╝', '═', '╚', '║'}
+    },
   },
 }
