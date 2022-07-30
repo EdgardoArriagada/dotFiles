111873bd7ec094c48b85da5ab392397a80da9388
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-07-29 22:45:42 -0400

    adjust pretty fold to avoid node keys bouncing around

--- nvim/.config/nvim/lua/zsb/setup/pretty-fold.lua
+++ nvim/.config/nvim/lua/zsb/setup/pretty-fold.lua
@@ -1,14 +1,29 @@
+-- if vim.v.foldlevel = 2 then spaces = 2
+-- if vim.v.foldlevel = 3 then spaces = 4
+-- if vim.v.foldlevel = 4 then spaces = 6
+-- if vim.v.foldlevel = 5 then spaces = 8
+-- ...
+local calculateSpaces = function(foldLevel)
+	if foldLevel <= 2 then
+		return foldLevel
+	end
+	local spaces = 2
+	for i = 3, foldLevel do
+		spaces = spaces + 2
+	end
+	return spaces
+end
+
 whenOk(require, "pretty-fold", function(prettyFold)
 	prettyFold.setup({
 		keep_indentation = false,
 		fill_char = "━",
 		sections = {
 			left = {
-				"⮀ ",
+				"⮀",
 				function()
-					return string.rep("·", vim.v.foldlevel)
+					return string.rep("·", calculateSpaces(vim.v.foldlevel) - 2)
 				end,
-				" ━┫",
 				"content",
 				"┣",
 			},
