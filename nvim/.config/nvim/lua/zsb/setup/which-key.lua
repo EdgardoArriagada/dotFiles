d9062e19a91f9fd244029ea65f4dcba20e0271e8
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-09-06 14:52:30 -0400

    rename how to stage hunk

--- nvim/.config/nvim/lua/zsb/setup/which-key.lua
+++ nvim/.config/nvim/lua/zsb/setup/which-key.lua
@@ -114,7 +114,7 @@ local mappings = {
 		p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
 		r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
 		R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
-		s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+		a = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Add Hunk" },
 		u = {
 			"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
 			"Undo Stage Hunk",
