1f249f834114df81b0f07e52c08d11439ac19c30
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-09-08 12:17:54 -0400

    customize telescope

--- nvim/.config/nvim/lua/zsb/setup/telescope.lua
+++ nvim/.config/nvim/lua/zsb/setup/telescope.lua
@@ -1,5 +1,15 @@
-if vim.g.vscode then return end
+if vim.g.vscode then
+	return
+end
+
+whenOk(require, "telescope", function(telescope)
+	telescope.setup({
+		defaults = {
+			path_display = { "truncate" },
+		},
+	})
+end)
 
 keymap.set("n", "<C-p>", function()
-  require("telescope.builtin").git_files()
+	require("telescope.builtin").git_files()
 end)
