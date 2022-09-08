fd1d7f164cca9a61fca3e18e65a97a76b47e1aeb
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-09-08 13:01:16 -0400

    add new hpcall and test it with telescope

--- nvim/.config/nvim/lua/zsb/setup/telescope.lua
+++ nvim/.config/nvim/lua/zsb/setup/telescope.lua
@@ -2,13 +2,16 @@ if vim.g.vscode then
 	return
 end
 
-whenOk(require, "telescope", function(telescope)
-	telescope.setup({
-		defaults = {
-			path_display = { "truncate" },
-		},
-	})
-end)
+hpcall(require, "telescope", {
+	onOk = function(telescope)
+		telescope.setup({
+			defaults = {
+				path_display = { "truncate" },
+			},
+		})
+	end,
+	onErr = "failed to setup telescope",
+})
 
 keymap.set("n", "<C-p>", function()
 	require("telescope.builtin").git_files()
--- nvim/.config/nvim/lua/zsb/utils/requirers.lua
+++ nvim/.config/nvim/lua/zsb/utils/requirers.lua
@@ -3,19 +3,33 @@
 -- when we find a way to exit original script using the callback
 --
 function withFallback(a, b, cb)
-  local ok, thing = pcall(a, b)
+	local ok, thing = pcall(a, b)
 
-  if not ok then cb() end
+	if not ok then
+		cb()
+	end
 
-  return thing
+	return thing
 end
-
 
 function whenOk(a, b, cb)
-  local ok, thing = pcall(a, b)
+	local ok, thing = pcall(a, b)
 
-  if not ok then return end
+	if not ok then
+		return
+	end
 
-  cb(thing)
+	cb(thing)
 end
 
+function hpcall(a, b, args)
+	local ok, thing = pcall(a, b)
+
+	local action = ok and "onOk" or "onErr"
+
+	if type(args[action]) == "function" then
+		args[action](thing, ok)
+	elseif type(args[action]) == "string" then
+		vim.notify(args[action])
+	end
+end
