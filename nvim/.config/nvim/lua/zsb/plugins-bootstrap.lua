972dc263a004dc632d1a5e66825badbb3d3db052
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-09-09 19:17:07 -0400

    deactivate automatic packer sync

--- nvim/.config/nvim/lua/zsb/plugins-bootstrap.lua
+++ nvim/.config/nvim/lua/zsb/plugins-bootstrap.lua
@@ -10,12 +10,12 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+-- vim.cmd([[
+--   augroup packer_user_config
+--     autocmd!
+--     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+--   augroup end
+-- ]])
 
 hpcall(require, "packer", {
 	onOk = function(packer)
