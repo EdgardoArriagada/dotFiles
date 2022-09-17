93ea5adc79937644f8c86a021f6e181bbfddfd6e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-09-17 19:57:20 -0300

    install and configure context

--- nvim/.config/nvim/lua/zsb/plugins.lua
+++ nvim/.config/nvim/lua/zsb/plugins.lua
@@ -27,6 +27,17 @@ function pluginsStartup(use)
 	use("kyazdani42/nvim-web-devicons")
 	use("moll/vim-bbye")
 
+	-- Context
+	use({
+		"nvim-treesitter/nvim-treesitter-context",
+		requires = {
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+		config = function()
+			require("treesitter-context").setup()
+		end,
+	})
+
 	-- Refactoring
 	use({
 		"ThePrimeagen/refactoring.nvim",
