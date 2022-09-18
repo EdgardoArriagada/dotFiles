b6507f1f9bf88b39c528f5eb6db969b28c4c2867
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-09-18 18:56:29 -0300

    apply a small refactor

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,19 +1,19 @@
 local M = require("utils")
 local visualizeApp, onAppLaunch = M.visualizeApp, M.onAppLaunch
 
+local ALACRITTY = "Alacritty"
+
 -- Switch alacritty
 hs.hotkey.bind("§", "§", function()
-	local APP_NAME = "Alacritty"
-
-	local alacritty = hs.application.get(APP_NAME)
+	local alacritty = hs.application.get(ALACRITTY)
 
 	if alacritty ~= nil and alacritty:isFrontmost() then
 		alacritty:hide()
 		return
 	end
 
-	if alacritty == nil and hs.application.launchOrFocus(APP_NAME) then
-		onAppLaunch(APP_NAME, visualizeApp)
+	if alacritty == nil and hs.application.launchOrFocus(ALACRITTY) then
+		onAppLaunch(ALACRITTY, visualizeApp)
 		return
 	end
 
