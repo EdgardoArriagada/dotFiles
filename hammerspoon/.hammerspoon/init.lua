d3e0cfbc0c32a07de4fe264005651577e32928d1
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-09-18 09:48:19 -0300

    refactor utils into its own file

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,60 +1,4 @@
-local spaces = require("hs.spaces")
-
-function OnAppLaunch(appName, callback)
-	local appWatcher = nil
-
-	appWatcher = hs.application.watcher.new(function(name, event, app)
-		if name == appName and event == hs.application.watcher.launched then
-			app:hide()
-			callback(app)
-			appWatcher:stop()
-		end
-	end)
-
-	appWatcher:start()
-end
-
-function SetWinFrame(win)
-	local scrFrame = hs.screen.mainScreen():fullFrame()
-
-	local winFrame = win:frame()
-	winFrame.w = scrFrame.w
-	winFrame.y = scrFrame.y
-	winFrame.x = scrFrame.x
-	win:setFrame(winFrame, 0)
-end
-
-function GetMainWindow(app)
-	local win = nil
-
-	while win == nil do
-		win = app:mainWindow()
-	end
-
-	return win
-end
-
-function VisualizeApp(app)
-	local currSpaceId = spaces.activeSpaceOnScreen()
-	local win = GetMainWindow(app)
-
-	local fullScreen = not win:isStandard()
-
-	if fullScreen then
-		hs.eventtap.keyStroke("cmd", "return", 0, app)
-	end
-
-	SetWinFrame(win)
-
-	spaces.moveWindowToSpace(win, currSpaceId)
-	spaces.spaceDisplay(currSpaceId)
-
-	if fullScreen then
-		hs.eventtap.keyStroke("cmd", "return", 0, app)
-	end
-
-	win:focus()
-end
+local m = require("utils")
 
 -- Switch alacritty
 hs.hotkey.bind("§", "§", function()
@@ -68,12 +12,12 @@ hs.hotkey.bind("§", "§", function()
 	end
 
 	if alacritty == nil and hs.application.launchOrFocus(APP_NAME) then
-		OnAppLaunch(APP_NAME, VisualizeApp)
+		m.onAppLaunch(APP_NAME, m.visualizeApp)
 		return
 	end
 
 	if alacritty ~= nil then
-		VisualizeApp(alacritty)
+		m.visualizeApp(alacritty)
 		return
 	end
 end)
---
+++ hammerspoon/.hammerspoon/utils/init.lua
@@ -0,0 +1,62 @@
+local spaces = require("hs.spaces")
+
+M = {}
+
+function M.onAppLaunch(appName, callback)
+	local appWatcher = nil
+
+	appWatcher = hs.application.watcher.new(function(name, event, app)
+		if name == appName and event == hs.application.watcher.launched then
+			app:hide()
+			callback(app)
+			appWatcher:stop()
+		end
+	end)
+
+	appWatcher:start()
+end
+
+function M.setWinFrame(win)
+	local scrFrame = hs.screen.mainScreen():fullFrame()
+	local winFrame = win:frame()
+
+	winFrame.w = scrFrame.w
+	winFrame.y = scrFrame.y
+	winFrame.x = scrFrame.x
+
+	win:setFrame(winFrame, 0)
+end
+
+function M.getMainWindow(app)
+	local win = nil
+
+	while win == nil do
+		win = app:mainWindow()
+	end
+
+	return win
+end
+
+function M.visualizeApp(app)
+	local currSpaceId = spaces.activeSpaceOnScreen()
+	local win = M.getMainWindow(app)
+
+	local fullScreen = not win:isStandard()
+
+	if fullScreen then
+		hs.eventtap.keyStroke("cmd", "return", 0, app)
+	end
+
+	M.setWinFrame(win)
+
+	spaces.moveWindowToSpace(win, currSpaceId)
+	spaces.spaceDisplay(currSpaceId)
+
+	if fullScreen then
+		hs.eventtap.keyStroke("cmd", "return", 0, app)
+	end
+
+	win:focus()
+end
+
+return M
