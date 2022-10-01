5efb14c6f70f93cf0aa6d22e9bd35e2a28077f6c
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-10-01 17:18:15 -0300

    update deprecated calls

--- nvim/.config/nvim/lua/zsb/lsp/handlers.lua
+++ nvim/.config/nvim/lua/zsb/lsp/handlers.lua
@@ -47,7 +47,7 @@ end
 
 local function lsp_highlight_document(client)
 	-- Set autocommands conditional on server_capabilities
-	if client.resolved_capabilities.document_highlight then
+	if client.server_capabilities.document_highlight then
 		vim.api.nvim_exec(
 			[[
       augroup lsp_document_highlight
@@ -93,7 +93,7 @@ local disabledForFormatting = {
 
 M.on_attach = function(client, bufnr)
 	if disabledForFormatting[client.name] then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.documentFormattingProvider = false
 	end
 
 	lsp_keymaps(bufnr)
