2ae045c146fcc9cdbbcb16e0b803b95c6700948e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-10-29 10:54:06 -0300

    indent miscellaneous file

--- nvim/.config/nvim/lua/zsb/utils/miscellaneous.lua
+++ nvim/.config/nvim/lua/zsb/utils/miscellaneous.lua
@@ -1,83 +1,85 @@
 function getDirectionalProps(direction)
-  if direction == 'j' then
-    return 1, line('$')
-  elseif direction == 'k' then
-    return -1, 1
-  end
+	if direction == "j" then
+		return 1, line("$")
+	elseif direction == "k" then
+		return -1, 1
+	end
 end
 
 function isEmptyString(input)
-  for c in input:gmatch"." do
-    if c ~= " " then return false end
-  end
+	for c in input:gmatch(".") do
+		if c ~= " " then
+			return false
+		end
+	end
 
-  return true
+	return true
 end
 
 function isEmptyLine(line)
-  return isEmptyString(vim.fn.getline(line))
+	return isEmptyString(vim.fn.getline(line))
 end
 
 function getFirstNoEmptyLine(direction, lineMarker)
-  local inc, endOfFile = getDirectionalProps(direction)
-  while lineMarker ~= endOfFile do
-    if not isEmptyLine(lineMarker) then
-      return lineMarker
-    end
+	local inc, endOfFile = getDirectionalProps(direction)
+	while lineMarker ~= endOfFile do
+		if not isEmptyLine(lineMarker) then
+			return lineMarker
+		end
 
-    lineMarker = lineMarker + inc
-  end
-  return lineMarker
+		lineMarker = lineMarker + inc
+	end
+	return lineMarker
 end
 
 function lookForIndentation(direction)
-  -- Go to beggin of line and add to jump list
-  execute('normal'..line('.')..'G^')
+	-- Go to beggin of line and add to jump list
+	execute("normal" .. line(".") .. "G^")
 
-  local lineMarker = getFirstNoEmptyLine(direction, line('.'))
+	local lineMarker = getFirstNoEmptyLine(direction, line("."))
 
-  execute('normal'..getSameIndentLine(direction, lineMarker)..'G^')
+	execute("normal" .. getSameIndentLine(direction, lineMarker) .. "G^")
 end
 
 function arrayElement(t)
-  local i = 0
+	local i = 0
 
-  return function()
-    i = i + 1
-    return t[i]
-  end
+	return function()
+		i = i + 1
+		return t[i]
+	end
 end
 
 function arrayElementBackward(t)
-  local i = #t + 1
+	local i = #t + 1
 
-  return function()
-    i = i - 1
-    return t[i]
-  end
+	return function()
+		i = i - 1
+		return t[i]
+	end
 end
 
 function toupleArrayElement(t)
-  local i = 0
+	local i = 0
 
-  return function()
-    i = i + 1
-    if t[i] then
-      return t[i][1], t[i][2]
-    end
-  end
+	return function()
+		i = i + 1
+		if t[i] then
+			return t[i][1], t[i][2]
+		end
+	end
 end
 
 function fromShell(command)
-  local handle = io.popen(command)
+	local handle = io.popen(command)
 
-  if handle then
-    local result = handle:read("*a")
-    handle:close()
-    return result
-  end
+	if handle then
+		local result = handle:read("*a")
+		handle:close()
+		return result
+	end
 
-  return nil
+	return nil
 end
 
 function escape_pattern(text)
