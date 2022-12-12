ae62d5491f141485ce50b49683ce01ae62c35433
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-12-11 21:14:27 -0300

    add a new visualInIndent custom plugin

---
+++ nvim/.config/nvim/lua/zsb/customPluggins/visualInIndent.lua
@@ -0,0 +1,18 @@
+keymap.set("v", "ii", function()
+	VisualInIndent()
+end, { noremap = true, silent = true })
+
+function VisualInIndent()
+	-- Go to beggin of line and add to jump list
+	Execute("normal<Esc>^m'")
+
+	local lineMarker = getFirstNoEmptyLine("k", line("."))
+
+	local lastSameIndentUp = getLesserIndent("k", lineMarker) + 1
+
+	Execute("normal" .. lastSameIndentUp .. "G^V")
+
+	local lastSameIndentDown = getLesserIndent("j", lineMarker) - 1
+
+	Execute("normal" .. lastSameIndentDown .. "G^")
+end
