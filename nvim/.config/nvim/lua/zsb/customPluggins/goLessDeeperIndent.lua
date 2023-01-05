d3c75c8e5691868f786356bd2686938fe836b014
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-01-04 21:49:28 -0300

    pretty goLessDeeperIndent

--- nvim/.config/nvim/lua/zsb/customPluggins/goLessDeeperIndent.lua
+++ nvim/.config/nvim/lua/zsb/customPluggins/goLessDeeperIndent.lua
@@ -1,26 +1,26 @@
-keymap.set({ 'o', 'n', 'v' }, '<bs>', function()
-  GoLessDeeperIndent('k')
+keymap.set({ "o", "n", "v" }, "<bs>", function()
+	GoLessDeeperIndent("k")
 end, { silent = true })
 
-keymap.set({ 'o', 'n', 'v' }, '<enter>', function()
-  GoLessDeeperIndent('j')
+keymap.set({ "o", "n", "v" }, "<enter>", function()
+	GoLessDeeperIndent("j")
 end, { silent = true })
 
 function GoLessDeeperIndent(direction)
-  -- Go to beggin of line and add to jump list
-  Execute("normal^m'")
+	-- Go to beggin of line and add to jump list
+	Execute("normal^m'")
 
-  local lineMarker = getFirstNoEmptyLine(direction, line('.'))
+	local lineMarker = getFirstNoEmptyLine(direction, line("."))
 
-  local originalInent = indent(lineMarker)
+	local originalInent = indent(lineMarker)
 
-  if originalInent == 0 then
-    local lastLine = getSameIndentLine(direction, lineMarker)
-    Execute('normal'..lastLine..'G^')
-    return
-  end
+	if originalInent == 0 then
+		local lastLine = getSameIndentLine(direction, lineMarker)
+		Execute("normal" .. lastLine .. "G^")
+		return
+	end
 
-  local lastMatchinLine = getLesserIndent(direction, lineMarker)
+	local lastMatchinLine = getLesserIndent(direction, lineMarker)
 
-  Execute('normal'..lastMatchinLine..'G^')
+	Execute("normal" .. lastMatchinLine .. "G^")
 end
