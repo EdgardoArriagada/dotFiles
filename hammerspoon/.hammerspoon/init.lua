1fe25821b987feb03ff60c2eb208a1269a4aff84
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-01-13 20:59:36 -0300

    weak focus slack

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -22,7 +22,7 @@ local cmd2 = { "command", "ctrl" }
 
 -- for more apps, see /Applications
 hs.hotkey.bind({}, "§", toggleApp("Alacritty"))
-hs.hotkey.bind(cmd2, "1", focusApp("Slack"))
+hs.hotkey.bind(cmd2, "1", weakFocus("Slack"))
 hs.hotkey.bind(cmd2, "2", focusApp("Google Chrome"))
 hs.hotkey.bind(cmd2, "3", weakFocus("IntelliJ IDEA"))
 hs.hotkey.bind(cmd2, "9", focusApp("Spotify"))
