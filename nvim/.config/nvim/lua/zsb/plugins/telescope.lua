2abb24515f52054c3ee521ef96a717ebb559c84e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-01-21 17:02:22 -0300

    apply a pretty format

--- nvim/.config/nvim/lua/zsb/plugins/telescope.lua
+++ nvim/.config/nvim/lua/zsb/plugins/telescope.lua
@@ -1,24 +1,24 @@
 return {
-		"nvim-telescope/telescope.nvim",
-    dependencies = {'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-fzf-native.nvim'},
-    opts = {
-			defaults = {
-				path_display = { "truncate" },
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
+	opts = {
+		defaults = {
+			path_display = { "truncate" },
+		},
+		extensions = {
+			fzf = {
+				fuzzy = true,
+				override_generic_sorter = true,
+				override_file_sorter = true,
+				case_mode = "smart_case",
 			},
-			extensions = {
-				fzf = {
-					fuzzy = true,
-					override_generic_sorter = true,
-					override_file_sorter = true,
-					case_mode = "smart_case",
-				},
-			},
-    },
-    keys = function() 
-      keymap.set("n", "<C-p>", function()
-        require("telescope.builtin").git_files({
-          show_untracked = true,
-        })
-      end)
-    end,
-	}
+		},
+	},
+	keys = function()
+		keymap.set("n", "<C-p>", function()
+			require("telescope.builtin").git_files({
+				show_untracked = true,
+			})
+		end)
+	end,
+}
