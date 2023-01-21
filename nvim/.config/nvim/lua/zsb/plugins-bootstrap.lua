cae2e8583c950af3e6ba9e5379fabde3201442f4
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-01-21 12:28:25 -0300

    install lazy nvim with one example

--- nvim/.config/nvim/lua/zsb/plugins-bootstrap.lua
+++ nvim/.config/nvim/lua/zsb/plugins-bootstrap.lua
@@ -1,41 +1,15 @@
-require("zsb.plugins")
-
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	packer_bootstrap =
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+print("lazy.nvim path: " .. lazypath)
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
--- vim.cmd([[
---   augroup packer_user_config
---     autocmd!
---     autocmd BufWritePost plugins.lua source <afile> | PackerSync
---   augroup end
--- ]])
-
-hpcall(require, "packer", {
-	onOk = function(packer)
-		-- Have packer use a popup window
-		packer.init({
-			display = {
-				open_fn = function()
-					return require("packer.util").float({ border = "rounded" })
-				end,
-			},
-		})
-
-		packer.startup(function(use)
-			pluginsStartup(use)
-
-			-- Automatically set up your configuration after cloning packer.nvim
-			-- Put this at the end after all plugins
-			if packer_bootstrap then
-				require("packer").sync()
-			end
-		end)
-	end,
-})
+require("lazy").setup("zsb.plugins")
