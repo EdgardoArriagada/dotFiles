80a07997abf368e104d8b2f8f16068526042922e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-01-21 21:55:39 -0300

    make hop config more standard

--- nvim/.config/nvim/lua/zsb/plugins/hop.lua
+++ nvim/.config/nvim/lua/zsb/plugins/hop.lua
@@ -1,8 +1,15 @@
 return {
 	"phaazon/hop.nvim",
 	branch = "v1", -- optional but strongly recommended
-	opts = { keys = "etovxqpdygfblzhckisuran" },
-	keys = function()
+	keys = "s",
+	config = function()
+		hpcall(require, "hop", {
+			onOk = function(hop)
+				hop.setup({ keys = "etovxqpdygfblzhckisuran" })
+			end,
+			onErr = "Failed to setup hop",
+		})
+
 		keymap.set("n", "s", function()
 			require("hop").hint_char2()
 		end, { noremap = false, silent = true })
