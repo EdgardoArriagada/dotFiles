5fd46b3b8e04e617599125e857a180b73593876d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-01-21 21:21:40 -0300

    refactor telescope to lazy load it

--- nvim/.config/nvim/lua/zsb/plugins/telescope.lua
+++ nvim/.config/nvim/lua/zsb/plugins/telescope.lua
@@ -1,20 +1,27 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
-	opts = {
-		defaults = {
-			path_display = { "truncate" },
-		},
-		extensions = {
-			fzf = {
-				fuzzy = true,
-				override_generic_sorter = true,
-				override_file_sorter = true,
-				case_mode = "smart_case",
-			},
-		},
-	},
-	keys = function()
+	keys = { "<C-p>" },
+	config = function()
+		hpcall(require, "telescope", {
+			onOk = function(telescope)
+				telescope.setup({
+					defaults = {
+						path_display = { "truncate" },
+					},
+					extensions = {
+						fzf = {
+							fuzzy = true,
+							override_generic_sorter = true,
+							override_file_sorter = true,
+							case_mode = "smart_case",
+						},
+					},
+				})
+			end,
+			onErr = "Failed to load telescope.nvim",
+		})
+
 		keymap.set("n", "<C-p>", function()
 			require("telescope.builtin").git_files({
 				show_untracked = true,
