d197e7c93246c215104ae3ffcfc2427d3a564ea2
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-01-21 21:59:55 -0300

    lazy load comment

--- nvim/.config/nvim/lua/zsb/plugins/comment.lua
+++ nvim/.config/nvim/lua/zsb/plugins/comment.lua
@@ -1,6 +1,7 @@
 return {
 	"numToStr/Comment.nvim",
 	dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+	keys = { { "gc", mode = "v" } },
 	opts = {
 		pre_hook = function(ctx)
 			local U = require("Comment.utils")
