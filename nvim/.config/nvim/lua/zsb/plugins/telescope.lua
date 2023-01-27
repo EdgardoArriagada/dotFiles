a57d0c7370766bdefd1dda4c4ae23528fa261610
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-01-26 22:58:42 -0300

    add leader to telescope to enable which-key things

--- nvim/.config/nvim/lua/zsb/plugins/telescope.lua
+++ nvim/.config/nvim/lua/zsb/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
-	keys = { "<C-p>" },
+	keys = { "<leader>", "<C-p>" },
 	config = function()
 		hpcall(require, "telescope", {
 			onOk = function(telescope)
