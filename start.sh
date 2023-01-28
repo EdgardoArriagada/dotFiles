acdcb257ee63456027c3935d2ac8859adcec3a54
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-01-27 22:55:41 -0300

    make start script more dynamic

--- start.sh
+++ start.sh
@@ -1,7 +1,46 @@
-if [[ "$(uname -s)" = "Darwin" ]]; then
-  # on MacOs
-  stow hammerspoon nvim
-else
-  # on Linux
-  stow nvim
-fi
+() (
+  local this="_dotfiles_setup_"
+  local isMacOs=`[[ "$(uname -s)" = "Darwin" ]] && printf 1`
+
+  ###### BEGIN CONFIGURATION ######
+  local commonPrograms=(
+    nvim
+    tmux
+  )
+
+  local macOsPrograms=(
+    hammerspoon
+  )
+
+  local linuxPrograms=()
+
+  ${this}.tmux() {
+    if (( isMacOs ))
+      then sed -i '.bak' '1s/^/[[ -z "$TMUX" ]] \&\& tmux new -A -s main\'$'\n/g' ~/.zshrc
+      else sed -i '1i[[ -z "$TMUX" ]] \&\& tmux new -A -s main' ~/.zshrc
+    fi
+  }
+
+  ######## END CONFIGURATION ######
+
+
+  ## Populate program list
+  local allPrograms=("${commonPrograms[@]}")
+
+  if (( isMacOs ))
+    then allPrograms+=("${macOsPrograms[@]}")
+    else allPrograms+=("${linuxPrograms[@]}")
+  fi
+
+  ## Link programs to dot files
+  stow "${allPrograms[@]}"
+
+  ## Run setup functions
+  for program in "${allPrograms[@]}"; do
+    if type ${this}.${program} > /dev/null
+      then ${this}.${program}
+    fi
+  done
+)
+
+
