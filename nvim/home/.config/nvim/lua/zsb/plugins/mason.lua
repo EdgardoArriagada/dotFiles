2fa0c47a59ea92ef0b545f7b3ba17c25729fa666
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-02-13 20:11:57 -0300

    apply a small refactor

--- nvim/home/.config/nvim/lua/zsb/plugins/mason.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/mason.lua
@@ -13,15 +13,6 @@ return {
 		require("mason-lspconfig").setup()
 
 		local on_attach = function(_client, bufnr)
-			local function buf_set_keymap(...)
-				Vim.api.nvim_buf_set_keymap(bufnr, ...)
-			end
-			local function buf_set_option(...)
-				Vim.api.nvim_buf_set_option(bufnr, ...)
-			end
-
-			buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
 			local opts = { noremap = true, silent = true }
 
 			Vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
@@ -33,44 +24,25 @@ return {
 			Vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 		end
 
-		local _capabilities = Vim.lsp.protocol.make_client_capabilities()
-		local capabilities = require("cmp_nvim_lsp").default_capabilities(_capabilities)
+		local capabilities = require("cmp_nvim_lsp").default_capabilities(Vim.lsp.protocol.make_client_capabilities())
 
 		local lspconfig = require("lspconfig")
 
-		lspconfig["pyright"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
+		local defaultServers = {
+			"pyright",
+			"cssls",
+			"emmet_ls",
+			"tsserver",
+			"lua_ls",
+			"bashls",
+			"gopls",
+		}
 
-		lspconfig["cssls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-
-		lspconfig["emmet_ls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-
-		lspconfig["tsserver"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-
-		lspconfig["lua_ls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-
-		lspconfig["bashls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-
-		lspconfig["gopls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
+		for _, server in pairs(defaultServers) do
+			lspconfig[server].setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+		end
 	end,
 }
