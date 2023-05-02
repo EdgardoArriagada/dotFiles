cd8be3969196991c298552b53a24aa72211e8111
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-05-02 18:18:09 -0400

    restore icons from hack

--- nvim/home/.config/nvim/lua/zsb/plugins/gitsigns.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/gitsigns.lua
@@ -11,13 +11,13 @@ return {
 			},
 			delete = {
 				hl = "GitSignsDelete",
-				text = "契",
+				text = "󰐊",
 				numhl = "GitSignsDeleteNr",
 				linehl = "GitSignsDeleteLn",
 			},
 			topdelete = {
 				hl = "GitSignsDelete",
-				text = "契",
+				text = "󰐊",
 				numhl = "GitSignsDeleteNr",
 				linehl = "GitSignsDeleteLn",
 			},
