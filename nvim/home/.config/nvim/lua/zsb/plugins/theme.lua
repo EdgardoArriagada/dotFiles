e2b2db63124d2e4de64783e1679aaa1b071d9a01
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-05-29 00:04:30 -0400

    fix telescope colors

--- nvim/home/.config/nvim/lua/zsb/plugins/theme.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/theme.lua
@@ -1,17 +1,27 @@
 return {
 	"eddyekofo94/gruvbox-flat.nvim",
-	config = function()
+	config = function(conf)
 		-- Change here --
 		---------- -------- -------- --------
 		local theme = "gruvbox-flat"
 		vim.g.gruvbox_flat_style = "dark"
 
-		-- more keys at https://github.com/eddyekofo94/gruvbox-flat.nvim/blob/master/lua/gruvbox/theme.lua
+		-- more color strings at https://github.com/eddyekofo94/gruvbox-flat.nvim/blob/master/lua/gruvbox/colors.lua
+		-- more keys to override at https://github.com/eddyekofo94/gruvbox-flat.nvim/blob/master/lua/gruvbox/theme.lua
+		local borderTheme = { fg = "purple", bg = "bg" }
 		vim.g.gruvbox_theme = {
 			NormalFloat = { bg = "#303030" },
 			Identifier = { fg = "blue" },
 			Function = { fg = "yellow" },
+			-- Telescope
+			TelescopePreviewBorder = borderTheme,
+			TelescopePreviewTitle = borderTheme,
+			TelescopePromptBorder = borderTheme,
+			TelescopePromptTitle = borderTheme,
+			TelescopeResultsBorder = borderTheme,
+			TelescopeResultsTitle = borderTheme,
 		}
+
 		---------- -------- -------- --------
 
 		local colorCmd = "colorscheme " .. theme
