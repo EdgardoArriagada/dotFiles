47f10b4f8b58caadf27f0bbee4bd3543784cad43
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-06-04 14:52:47 -0400

    add instructions on how to repare clipboard-image

--- nvim/home/.config/nvim/lua/zsb/plugins/init.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/init.lua
@@ -12,7 +12,12 @@ return {
 	{ "github/copilot.vim" },
 	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 	{ "windwp/nvim-ts-autotag", dependencies = { "nvim-treesitter/nvim-treesitter" } },
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", keys = "<C-p>" },
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+		keys = "<C-p>",
+	},
 	{ "moll/vim-bbye" },
+	-- if it doesn't work, follow https://github.com/ekickx/clipboard-image.nvim/issues/50
 	{ "ekickx/clipboard-image.nvim", ft = { "markdown" } },
 }
