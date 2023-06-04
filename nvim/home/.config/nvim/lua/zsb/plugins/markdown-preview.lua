05164bc14919336ca13879879208552757db2195
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-06-04 14:50:39 -0400

    add instructions on how to repace markdown preview

--- nvim/home/.config/nvim/lua/zsb/plugins/markdown-preview.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/markdown-preview.lua
@@ -1,3 +1,5 @@
+-- if it doesnt install, you can manually do so
+-- cd ~/.local/share/nvim/lazy/markdown-preview.nvim && npm install
 return {
 	"iamcco/markdown-preview.nvim",
 	build = "cd app && npm install",
