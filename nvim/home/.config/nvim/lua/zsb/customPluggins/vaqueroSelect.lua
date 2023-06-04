6492b0341bff216feda938ee7a8f615fbc9c562e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-06-04 16:28:18 -0400

    apply some formatting

--- nvim/home/.config/nvim/lua/zsb/customPluggins/vaqueroSelect.lua
+++ nvim/home/.config/nvim/lua/zsb/customPluggins/vaqueroSelect.lua
@@ -1,14 +1,14 @@
-keymap.set('v', 'q', function()
-  Execute('normal<Esc>')
-  VaqueroSelect()
+keymap.set("v", "q", function()
+	Execute("normal<Esc>")
+	VaqueroSelect()
 end, { noremap = true, silent = true })
 
-keymap.set('o', 'q', function()
-  VaqueroSelect()
+keymap.set("o", "q", function()
+	VaqueroSelect()
 end, { noremap = true, silent = true })
 
 function VaqueroSelect()
-  GoLessDeeperIndent('k')
-  Execute('normal!V')
-  lookForIndentation('j')
+	GoLessDeeperIndent("k")
+	Execute("normal!V")
+	lookForIndentation("j")
 end
