426be7aa25477afacc8ae57905d9556b74e3ac8d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-06-04 16:39:06 -0400

    fix gq

--- nvim/home/.config/nvim/lua/zsb/autocmd.lua
+++ nvim/home/.config/nvim/lua/zsb/autocmd.lua
@@ -8,6 +8,13 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	group = group,
 })
 
+-- Use internal formatting for bindings like gq.
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		vim.bo[args.buf].formatexpr = nil
+	end,
+})
+
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "json",
 	callback = function()
