45ff11949b74ae5fb45aa2b8e136f0f1dc0bfed5
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-07-05 09:33:07 -0400

    install dadbod and sync plugins

---
+++ nvim/home/.config/nvim/lua/zsb/plugins/vim-dadbod-ui.lua
@@ -0,0 +1,56 @@
+return {
+	"kristijanhusak/vim-dadbod-ui",
+	dependencies = {
+		"tpope/vim-dadbod",
+		"kristijanhusak/vim-dadbod-completion",
+		"tpope/vim-dotenv",
+	},
+	keys = { { "<leader><leader>db", ":tab DBUI<cr>", desc = "Open Database client" } },
+	init = function()
+		vim.g.db_ui_use_nerd_fonts = 1
+		vim.g.db_ui_show_database_icon = 1
+		vim.g.db_ui_force_echo_notifications = 1
+		vim.g.db_ui_win_position = "left"
+		vim.g.db_ui_winwidth = 80
+
+		vim.g.db_ui_table_helpers = {
+			mysql = {
+				Count = "select count(1) from {optional_schema}{table}",
+				Explain = "EXPLAIN {last_query}",
+			},
+			sqlite = {
+				Describe = "PRAGMA table_info({table})",
+			},
+		}
+
+		-- https://www.nerdfonts.com/cheat-sheet
+
+		vim.g.db_ui_icons = {
+			expanded = {
+				db = "▾ ",
+				buffers = "▾ ",
+				saved_queries = "▾ ",
+				schemas = "▾ ",
+				schema = "▾ 󰠵",
+				tables = "▾ 󱏈",
+				table = "▾ 󰓫",
+			},
+			collapsed = {
+				db = "▸ ",
+				buffers = "▸ ",
+				saved_queries = "▸ ",
+				schemas = "▸ ",
+				schema = "▸ 󰠵",
+				tables = "▸ 󱏈",
+				table = "▸ 󰓫",
+			},
+			saved_query = "",
+			new_query = "󰓰",
+			tables = "󱏈",
+			buffers = "",
+			add_connection = "󰆺",
+			connection_ok = "✓",
+			connection_error = "✕",
+		}
+	end,
+}
