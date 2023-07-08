473f0c3c11e9b7bff0df17434c95fae93d6f1f99
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-07-08 16:11:43 -0400

    refactor dbui and modify mappings

--- nvim/home/.config/nvim/ftplugin/dbui.lua
+++ nvim/home/.config/nvim/ftplugin/dbui.lua
@@ -1,3 +1,4 @@
 -- https://github.com/kristijanhusak/vim-dadbod-ui#mappings
-vim.keymap.set("n", "l", "<PLUG>(DBUI_SelectLine)", { buffer = 0 })
-vim.keymap.set("n", "h", "<PLUG>(DBUI_SelectLine)", { buffer = 0 })
+local toggleDrawerWith = MakeMultiMapHelper("n", "<PLUG>(DBUI_SelectLine)", { buffer = 0 })
+
+toggleDrawerWith({ "l", "h", "<CR>" })
