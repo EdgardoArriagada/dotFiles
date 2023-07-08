c7acf68e0c930a10988e1d26fe5089245a38d572
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-07-08 16:06:18 -0400

    improve keymaps for dbui

---
+++ nvim/home/.config/nvim/ftplugin/dbui.lua
@@ -0,0 +1,3 @@
+-- https://github.com/kristijanhusak/vim-dadbod-ui#mappings
+vim.keymap.set("n", "l", "<PLUG>(DBUI_SelectLine)", { buffer = 0 })
+vim.keymap.set("n", "h", "<PLUG>(DBUI_SelectLine)", { buffer = 0 })
