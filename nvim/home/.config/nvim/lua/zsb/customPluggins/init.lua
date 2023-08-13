93672731d56dd63c41025d2f5deeff007dadb788
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-08-13 15:08:28 -0400

    migrate plugin from personal configs to vqs

--- nvim/home/.config/nvim/lua/zsb/customPluggins/init.lua
+++ nvim/home/.config/nvim/lua/zsb/customPluggins/init.lua
@@ -1,5 +1,4 @@
 require("zsb.customPluggins.goLessDeeperIndent")
-require("zsb.customPluggins.powerSelection")
 require("zsb.customPluggins.vaqueroSelect")
 require("zsb.customPluggins.smartTab")
 require("zsb.customPluggins.toggleJsFile")
---
+++ nvim/home/.config/nvim/lua/zsb/plugins/vaquero-shoot.lua
@@ -0,0 +1,29 @@
+return {
+	"EdgardoArriagada/vaquero-shoot.nvim",
+	dir = "~/projects/per/vaquero-shoot.nvim",
+
+	config = function()
+		hpcall(require, "vaquero-shoot", {
+			onOk = function(vqs)
+				-- enclosing
+				keymap.set("n", "W", function()
+					vqs.beginEnclosingSelection()
+				end)
+
+				keymap.set("v", "W", function()
+					vqs.cycleEnclosingSelection()
+				end)
+
+				-- quotes
+				keymap.set({ "o", "v" }, "'", function()
+					if vqs.hasQuotesSelection() then
+						vqs.cycleQuotesSelection()
+					else
+						vqs.beginQuotesSelection()
+					end
+				end)
+			end,
+			onErr = "Failed to load vaquero-shoot.nvim",
+		})
+	end,
+}
