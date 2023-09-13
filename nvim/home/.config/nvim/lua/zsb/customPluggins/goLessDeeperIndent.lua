1d0c4413ec4e6091c3804c38eeab8ed8782a0cb6
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-09-13 13:19:46 -0300

    make enter unfold lines as well

--- nvim/home/.config/nvim/lua/zsb/customPluggins/goLessDeeperIndent.lua
+++ nvim/home/.config/nvim/lua/zsb/customPluggins/goLessDeeperIndent.lua
@@ -1,26 +1,31 @@
 kset({ "o", "n", "v" }, "<bs>", function()
-	GoLessDeeperIndent("k")
+  GoLessDeeperIndent("k")
 end, { silent = true })
 
 kset({ "o", "n", "v" }, "<enter>", function()
-	GoLessDeeperIndent("j")
+  -- I use enter to unfold lines as well
+  if IsCurrentLineFolded() then
+    Execute("normal! zO")
+  else
+    GoLessDeeperIndent("j")
+  end
 end, { silent = true })
 
 function GoLessDeeperIndent(direction)
-	-- Go to beggin of line and add to jump list
-	Execute("normal^m'")
+  -- Go to beggin of line and add to jump list
+  Execute("normal^m'")
 
-	local lineMarker = getFirstNoEmptyLine(direction, line("."))
+  local lineMarker = getFirstNoEmptyLine(direction, line("."))
 
-	local originalInent = indent(lineMarker)
+  local originalInent = indent(lineMarker)
 
-	if originalInent == 0 then
-		local lastLine = getSameIndentLine(direction, lineMarker)
-		Execute("normal" .. lastLine .. "G^")
-		return
-	end
+  if originalInent == 0 then
+    local lastLine = getSameIndentLine(direction, lineMarker)
+    Execute("normal" .. lastLine .. "G^")
+    return
+  end
 
-	local lastMatchinLine = getLesserIndent(direction, lineMarker)
+  local lastMatchinLine = getLesserIndent(direction, lineMarker)
 
-	Execute("normal" .. lastMatchinLine .. "G^")
+  Execute("normal" .. lastMatchinLine .. "G^")
 end
