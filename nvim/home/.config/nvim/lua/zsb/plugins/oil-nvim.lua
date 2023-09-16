315b9ebfd5e7f54a5630de3be78fb5eb2d3f7049
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-09-16 13:50:28 -0300

    install and configure oil nvim

---
+++ nvim/home/.config/nvim/lua/zsb/plugins/oil-nvim.lua
@@ -0,0 +1,17 @@
+return {
+	"stevearc/oil.nvim",
+	opts = {},
+
+	config = function()
+		Hpcall(require, "oil", {
+			onOk = function(oil)
+				oil.setup()
+				kset("n", "-", oil.open, { noremap = true })
+			end,
+			onErr = "Failed to load vaquero-shoot.nvim",
+		})
+	end,
+
+	-- Optional dependencies
+	dependencies = { "kyazdani42/nvim-web-devicons" },
+}
