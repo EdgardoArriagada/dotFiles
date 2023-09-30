c9a00b7a17b005ecd5fbcea338a3566112d02df0
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-09-30 19:28:16 -0300

    allow pasting using c-v in neovide

--- nvim/home/.config/nvim/lua/zsb/neovide-config.lua
+++ nvim/home/.config/nvim/lua/zsb/neovide-config.lua
@@ -1,3 +1,10 @@
 vim.o.guifont = "Hack Nerd Font:h21"
 vim.g.neovide_scroll_animation_length = 0.6
 vim.g.neovide_fullscreen = true
+
+-- Allow clipboard copy paste in neovim
+vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
