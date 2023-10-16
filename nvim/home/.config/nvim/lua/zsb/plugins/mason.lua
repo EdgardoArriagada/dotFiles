929c7643474488c20f892595cb488d1897dfdce6
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-10-15 22:55:34 -0300

    fix defaultSetUp

--- nvim/home/.config/nvim/lua/zsb/plugins/mason.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/mason.lua
@@ -7,6 +7,18 @@ local function organize_imports()
 	vim.lsp.buf.execute_command(params)
 end
 
+local on_attach = function(_, bufnr)
+	local opts = { noremap = true, silent = true }
+
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+end
+
 return {
 	"williamboman/mason.nvim",
 	dependencies = {
@@ -18,23 +30,11 @@ return {
 
 		require("mason-lspconfig").setup()
 
-		local on_attach = function(_client, bufnr)
-			local opts = { noremap = true, silent = true }
-
-			vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-			vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-			vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-			vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-			vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-			vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-			vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-		end
-
 		local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 		local defaultSetUp = {
-			on_attach,
-			capabilities,
+			on_attach = on_attach,
+			capabilities = capabilities,
 		}
 
 		local lspconfig = require("lspconfig")
