c8fd83a25fc02d97d0495719a22f059ff918f3da
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-11-25 12:34:09 -0300

    apply a small refactor

--- nvim/home/.config/nvim/lua/zsb/autocmd.lua
+++ nvim/home/.config/nvim/lua/zsb/autocmd.lua
@@ -1,13 +1,12 @@
 --[[ if vim.g.vscode then return end ]]
 local autocmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-local defaultGroup = augroup("zsb", { clear = true })
+local group = vim.api.nvim_create_augroup("Zsb", { clear = true })
 
 autocmd("BufWritePre", {
 	callback = function()
 		vim.lsp.buf.format({ async = true })
 	end,
-	group = defaultGroup,
+	group = group,
 })
 
 -- Use internal formatting for bindings like gq.
@@ -15,6 +14,7 @@ autocmd("LspAttach", {
 	callback = function(args)
 		vim.bo[args.buf].formatexpr = nil
 	end,
+	group = group,
 })
 
 autocmd("FileType", {
@@ -25,13 +25,13 @@ autocmd("FileType", {
 			Hpcall(Execute, "normal!zA", { onErr = 'failed to execute ":normal!zA"' })
 		end)
 	end,
-	group = defaultGroup,
+	group = group,
 })
 
 -- Triger `autoread` when files changes on disk
 autocmd(
 	{ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" },
-	{ command = [[if mode() != 'c' | checktime | endif]], group = defaultGroup }
+	{ command = [[if mode() != 'c' | checktime | endif]], group = group }
 )
 
 -- Notification after file change
@@ -39,5 +39,5 @@ autocmd("FileChangedShellPost", {
 	callback = function()
 		Warn("File changed on disk. Buffer reloaded.")
 	end,
-	group = defaultGroup,
+	group = group,
 })
