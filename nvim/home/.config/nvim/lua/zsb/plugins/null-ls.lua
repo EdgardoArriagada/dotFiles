59664d7032008ba1397fa8def493f1e80d793b9d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-01-06 15:36:38 -0300

    make a small refactor

--- nvim/home/.config/nvim/lua/zsb/plugins/null-ls.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/null-ls.lua
@@ -1,29 +1,28 @@
 return {
-	"jose-elias-alvarez/null-ls.nvim",
-	dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim", "jay-babu/mason-null-ls.nvim" },
+	"nvimtools/none-ls.nvim",
+	dependencies = { "jay-babu/mason-null-ls.nvim" },
 	config = function()
 		Hpcall(require, "null-ls", {
 			onOk = function(null_ls)
 				-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-				local formatting = null_ls.builtins.formatting
+				local f = null_ls.builtins.formatting
 				-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-				local diagnostics = null_ls.builtins.diagnostics
+				local d = null_ls.builtins.diagnostics
 
 				null_ls.setup({
 					debug = false,
 					sources = {
 						-- javascript
-						--[[ formatting.prettierd, -- https://github.com/fsouza/prettierd#installation-guide ]]
-						formatting.prettier,
-						diagnostics.eslint_d,
+						--[[ f.prettierd, -- https://github.com/fsouza/prettierd#installation-guide ]]
+						f.prettier,
+						d.eslint_d,
 						-- rust
-						formatting.rustfmt,
-						-- [diagnostics: rust-analyzer LSP]
+						f.rustfmt,
 						-- python
-						formatting.black,
-						diagnostics.flake8,
+						f.black,
+						d.flake8,
 						-- lua
-						formatting.stylua,
+						f.stylua,
 					},
 				})
 			end,
