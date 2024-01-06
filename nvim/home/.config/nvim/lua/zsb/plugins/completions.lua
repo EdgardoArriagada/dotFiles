f817a30c26fe6bdab55f0203581cfd58d82cf333
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-01-06 16:09:46 -0300

    move completions to their own files

---
+++ nvim/home/.config/nvim/lua/zsb/plugins/completions.lua
@@ -0,0 +1,9 @@
+return {
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-nvim-lua" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "saadparwaiz1/cmp_luasnip" }, -- snippet completions
+}
--- nvim/home/.config/nvim/lua/zsb/plugins/init.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/init.lua
@@ -1,22 +1,13 @@
 return {
-	-- completion plugins
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/cmp-nvim-lua" },
-	{ "hrsh7th/cmp-buffer" },
-	{ "hrsh7th/cmp-path" },
-	{ "hrsh7th/cmp-cmdline" },
-	{ "hrsh7th/nvim-cmp" },
-	{ "saadparwaiz1/cmp_luasnip" }, -- snippet completions
-	-- end completion
-	{ "github/copilot.vim" },
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	{ "windwp/nvim-ts-autotag", dependencies = { "nvim-treesitter/nvim-treesitter" } },
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
-		keys = "<C-p>",
-	},
-	{ "moll/vim-bbye" },
-	-- if it doesn't work, follow https://github.com/ekickx/clipboard-image.nvim/issues/50
-	{ "ekickx/clipboard-image.nvim", ft = { "markdown" } },
+  { "github/copilot.vim" },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "windwp/nvim-ts-autotag",          dependencies = { "nvim-treesitter/nvim-treesitter" } },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    keys = "<C-p>",
+  },
+  { "moll/vim-bbye" },
+  -- if it doesn't work, follow https://github.com/ekickx/clipboard-image.nvim/issues/50
+  { "ekickx/clipboard-image.nvim", ft = { "markdown" } },
 }
