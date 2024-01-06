e99a12e770528ee6760206cbf10f206edb2f75cf
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-01-06 15:35:03 -0300

    make ensure_installed a few alwayz cuz it will auto install

--- nvim/home/.config/nvim/lua/zsb/plugins/nvim-treesitter.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/nvim-treesitter.lua
@@ -7,8 +7,7 @@ return {
 				configs.setup({
 
 					-- A list of parser names, or "all" (the five listed parsers should always be installed)
-					ensure_installed = "all",
-					--[[ ensure_installed = { "lua", "vim", "vimdoc", "query" }, ]]
+					ensure_installed = { "lua", "vim", "vimdoc", "query" },
 
 					-- Install parsers synchronously (only applied to `ensure_installed`)
 					sync_install = false,
