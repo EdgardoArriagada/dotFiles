287cc69d93379b093b8e0df9a18da670dbf941d8
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-01-14 11:04:46 -0300

    lazy load completions

--- nvim/home/.config/nvim/lua/zsb/plugins/lsp-config.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/lsp-config.lua
@@ -54,6 +54,7 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		event = "InsertEnter",
 		config = function()
 			local capabilities =
 				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
