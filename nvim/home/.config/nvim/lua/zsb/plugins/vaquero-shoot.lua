78c96d23acdd36d9f1d31096adf5f9440614da38
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-01-14 10:45:47 -0300

    lazy load vqs

--- nvim/home/.config/nvim/lua/zsb/plugins/vaquero-shoot.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/vaquero-shoot.lua
@@ -1,18 +1,19 @@
 return {
-	"EdgardoArriagada/vaquero-shoot.nvim",
-	config = Config("vaquero-shoot", function(vqs)
-		-- enclosing
-		kset("n", "W", function()
-			vqs.beginEnclosingSelection()
-		end)
+  "EdgardoArriagada/vaquero-shoot.nvim",
+  keys = { "W", { "'", mode = "v" } },
+  config = Config("vaquero-shoot", function(vqs)
+    -- enclosing
+    kset("n", "W", function()
+      vqs.beginEnclosingSelection()
+    end)
 
-		kset("v", "W", function()
-			vqs.cycleEnclosingSelection()
-		end)
+    kset("v", "W", function()
+      vqs.cycleEnclosingSelection()
+    end)
 
-		-- quotes
-		kset({ "o", "v" }, "'", function()
-			vqs.quotesSelection()
-		end)
-	end),
+    -- quotes
+    kset({ "o", "v" }, "'", function()
+      vqs.quotesSelection()
+    end)
+  end),
 }
