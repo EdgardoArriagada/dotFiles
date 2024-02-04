20694635e9a0033ad13475684f0add9a32244433
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-02-04 11:18:30 -0300

    apply a small refactor

--- hammerspoon/home/.hammerspoon/init.lua
+++ hammerspoon/home/.hammerspoon/init.lua
@@ -1,17 +1,13 @@
-local utils = require("utils")
-local hideAllApps = utils.hideAllApps
-local focusApp = utils.focusApp
-local toggleApp = utils.toggleApp
-local weakFocus = utils.weakFocus
+local u = require("utils")
 
 local cmd2 = { "command", "ctrl" }
 
 -- for more apps, run
 -- $ ls /Applications
-hs.hotkey.bind({}, "§", toggleApp("Alacritty"))
-hs.hotkey.bind(cmd2, "1", weakFocus("Slack"))
-hs.hotkey.bind(cmd2, "2", focusApp("Google Chrome"))
-hs.hotkey.bind(cmd2, "3", weakFocus("IntelliJ IDEA"))
-hs.hotkey.bind(cmd2, "4", weakFocus("Insomnia"))
-hs.hotkey.bind(cmd2, "9", focusApp("Spotify"))
-hs.hotkey.bind(cmd2, "h", hideAllApps)
+hs.hotkey.bind({}, "§", u.toggleApp("Alacritty"))
+hs.hotkey.bind(cmd2, "1", u.weakFocus("Slack"))
+hs.hotkey.bind(cmd2, "2", u.focusApp("Google Chrome"))
+hs.hotkey.bind(cmd2, "3", u.weakFocus("IntelliJ IDEA"))
+hs.hotkey.bind(cmd2, "4", u.weakFocus("Insomnia"))
+hs.hotkey.bind(cmd2, "9", u.focusApp("Spotify"))
+hs.hotkey.bind(cmd2, "h", u.hideAllApps)
