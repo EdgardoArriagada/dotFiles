96b61c6978e6456b653851b4a386656feb49f921
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-02-09 18:51:01 -0300

    update comment

--- nvim/home/.config/nvim/lua/zsb/plugins/vaquero-shoot.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/vaquero-shoot.lua
@@ -1,20 +1,20 @@
 return {
-  "EdgardoArriagada/vaquero-shoot.nvim",
-  --[[ dir = "~/projects/per/vaquero-shoot.nvim ]]
-  keys = { "W", { "W", mode = "v" }, { "'", mode = "v" } },
-  config = Config("vaquero-shoot", function(vqs)
-    -- enclosing
-    kset("n", "W", function()
-      vqs.beginEnclosingSelection()
-    end)
+	"EdgardoArriagada/vaquero-shoot.nvim",
+	--[[ dir = "~/projects/per/vaquero-shoot.nvim", ]]
+	keys = { "W", { "W", mode = "v" }, { "'", mode = "v" } },
+	config = Config("vaquero-shoot", function(vqs)
+		-- enclosing
+		kset("n", "W", function()
+			vqs.beginEnclosingSelection()
+		end)
 
-    kset("v", "W", function()
-      vqs.cycleEnclosingSelection()
-    end)
+		kset("v", "W", function()
+			vqs.cycleEnclosingSelection()
+		end)
 
-    -- quotes
-    kset({ "o", "v" }, "'", function()
-      vqs.quotesSelection()
-    end)
-  end),
+		-- quotes
+		kset({ "o", "v" }, "'", function()
+			vqs.quotesSelection()
+		end)
+	end),
 }
