aa49dcc6ec1689f0322c3f43e5222b3cf7305f74
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-01 09:21:39 -0300

    update vqs

--- nvim/home/.config/nvim/lua/zsb/plugins/vaquero-shoot.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/vaquero-shoot.lua
@@ -1,20 +1,32 @@
 return {
-	"EdgardoArriagada/vaquero-shoot.nvim",
-	--[[ dir = "~/projects/per/vaquero-shoot.nvim", ]]
-	keys = { "W", { "W", mode = "v" }, { "'", mode = "v" } },
-	config = Config("vaquero-shoot", function(vqs)
-		-- enclosing
-		kset("n", "W", function()
-			vqs.beginEnclosingSelection()
-		end)
+  "EdgardoArriagada/vaquero-shoot.nvim",
+  --[[ dir = "~/projects/per/vaquero-shoot.nvim", ]]
+  keys = { "W", "E", { "W", mode = "v" }, { "E", mode = "v" }, { "'", mode = "v" }, { '"', mode = "v" } },
+  config = Config("vaquero-shoot", function(vqs)
+    -- enclosing
+    kset("n", "E", function()
+      vqs.beginEnclosingSelection()
+    end)
 
-		kset("v", "W", function()
-			vqs.cycleEnclosingSelection()
-		end)
+    kset("v", "E", function()
+      vqs.cycleEnclosingSelection()
+    end)
 
-		-- quotes
-		kset({ "o", "v" }, "'", function()
-			vqs.quotesSelection()
-		end)
-	end),
+    kset("n", "W", function()
+      vqs.beginEnclosingSelectionBackwards()
+    end)
+
+    kset("v", "W", function()
+      vqs.cycleEnclosingSelectionBackwards()
+    end)
+
+    -- quotes
+    kset({ "o", "v" }, "'", function()
+      vqs.quotesSelection()
+    end)
+
+    kset({ "o", "v" }, '"', function()
+      vqs.quotesSelectionBackwards()
+    end)
+  end),
 }
