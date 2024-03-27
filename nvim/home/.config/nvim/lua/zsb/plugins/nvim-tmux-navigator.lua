286fce1920b496b773fd6a5f5968468c667534c1
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-27 14:37:11 -0300

    lazy load tmux navigator

--- nvim/home/.config/nvim/lua/zsb/plugins/nvim-tmux-navigator.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/nvim-tmux-navigator.lua
@@ -1,5 +1,6 @@
 return {
 	"christoomey/vim-tmux-navigator",
+	keys = { "<C-h>", "<C-j>", "<C-k>", "<C-l>" },
 	config = function()
 		kset("n", "C-h", ":TmuxNavigateLeft<CR>")
 		kset("n", "C-j", ":TmuxNavigateDown<CR>")
