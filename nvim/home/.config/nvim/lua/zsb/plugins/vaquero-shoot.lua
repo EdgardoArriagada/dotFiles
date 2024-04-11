539b1ed476f998bee76375ec87a99c0095746480
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-04-10 21:48:18 -0400

    apply a small refactor

--- nvim/home/.config/nvim/lua/zsb/plugins/vaquero-shoot.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/vaquero-shoot.lua
@@ -5,16 +5,12 @@ return {
 	config = Config("vaquero-shoot", function(vqs)
 		-- enclosing
 		kset("n", "E", vqs.beginEnclosingSelection)
-
 		kset("v", "E", vqs.cycleEnclosingSelection)
-
 		kset("n", "W", vqs.beginEnclosingSelectionBackwards)
-
 		kset("v", "W", vqs.cycleEnclosingSelectionBackwards)
 
 		-- quotes
 		kset({ "o", "v" }, "'", vqs.quotesSelection)
-
 		kset({ "o", "v" }, '"', vqs.quotesSelectionBackwards)
 	end),
 }
