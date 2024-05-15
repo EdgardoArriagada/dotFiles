2f3c8494b1a5992715b0647712f2f49864dcfd4d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-05-15 13:12:31 -0400

    improve harpoong nav shortcut

--- nvim/home/.config/nvim/lua/zsb/plugins/harpoon.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/harpoon.lua
@@ -1,5 +1,9 @@
 return {
-	keys = { "<leader>" },
+	keys = { "<leader>", "(", ")" },
 	"ThePrimeagen/harpoon",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	config = Config("harpoon.ui", function(harpoon_ui)
+		kset("n", "(", harpoon_ui.nav_prev)
+		kset("n", ")", harpoon_ui.nav_next)
+	end),
 }
