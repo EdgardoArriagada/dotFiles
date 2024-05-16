49954658e09bb9c0a0ea99b0805f1e42ebbfa135
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-05-16 15:50:30 -0400

    fix file toggler

--- nvim/home/.config/nvim/lua/zsb/customPluggins/toggleJsFile.lua
+++ nvim/home/.config/nvim/lua/zsb/customPluggins/toggleJsFile.lua
@@ -1,3 +1,10 @@
+local FILE_EXTENSIONS = {
+	"js",
+	"jsx",
+	"ts",
+	"tsx",
+}
+
 local function isTestFileJs()
 	local fileName = vim.fn.expand("%:t")
 	local extension = vim.fn.expand("%:e")
@@ -10,6 +17,15 @@ local function getTestFileJs()
 	local extension = vim.fn.expand("%:e")
 	local fileDir = vim.fn.expand("%:h")
 
+	for _, ex in ipairs(FILE_EXTENSIONS) do
+		local testFileName = fileName:gsub("%." .. extension .. "$", "%.spec%." .. ex)
+		local isReadable = vim.fn.filereadable(fileDir .. "/__tests__/" .. testFileName) ~= 0
+
+		if isReadable then
+			return fileDir .. "/__tests__/" .. testFileName
+		end
+	end
+
 	local testFileName = fileName:gsub("%." .. extension .. "$", "%.spec%." .. extension)
 
 	return fileDir .. "/__tests__/" .. testFileName
@@ -19,6 +35,17 @@ local function getProductionCodeFileJS()
 	local fileName = vim.fn.expand("%:t")
 	local extension = vim.fn.expand("%:e")
 	local testFileDir = vim.fn.expand("%:h")
+
+	for _, ex in ipairs(FILE_EXTENSIONS) do
+		local productionCodeFileName = fileName:gsub("%.spec%." .. extension .. "$", "%." .. ex)
+		local productionCodeFileDir = testFileDir:gsub("__tests__", "")
+		local result = productionCodeFileDir .. productionCodeFileName
+		local isReadable = vim.fn.filereadable(result) ~= 0
+
+		if isReadable then
+			return result
+		end
+	end
 
 	local productionCodeFileName = fileName:gsub("%.spec%." .. extension .. "$", "%." .. extension)
 	local productionCodeFileDir = testFileDir:gsub("__tests__", "")
