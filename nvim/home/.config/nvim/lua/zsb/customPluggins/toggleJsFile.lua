34e6acb96bcff090b17cf915ae3d340e8c44078f
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-05-16 20:30:35 -0400

    apply a small refactor

--- nvim/home/.config/nvim/lua/zsb/customPluggins/toggleJsFile.lua
+++ nvim/home/.config/nvim/lua/zsb/customPluggins/toggleJsFile.lua
@@ -7,25 +7,26 @@ local JS_EXTENSIONS = {
 
 local function isTestFileJs()
 	local fileName = vim.fn.expand("%:t")
-	local extension = vim.fn.expand("%:e")
+	local ft = vim.fn.expand("%:e")
 
-	return string.match(fileName, "%.spec%." .. extension .. "$")
+	return string.match(fileName, "%.spec%." .. ft .. "$")
 end
 
 local function getTestFileJs()
 	local fileName = vim.fn.expand("%:t")
 	local ft = vim.fn.expand("%:e")
-	local fileDir = vim.fn.expand("%:h")
-	local testDir = fileDir .. "/__tests__/"
+	local testDir = vim.fn.expand("%:h") .. "/__tests__/"
 	local endFile = "%." .. ft .. "$"
 
 	for _, ex in ipairs(JS_EXTENSIONS) do
 		local extension = "%.spec%." .. ex
 		local testFileName = fileName:gsub(endFile, extension)
-		local isReadable = vim.fn.filereadable(testDir .. testFileName) ~= 0
+		local result = testDir .. testFileName
+
+		local isReadable = vim.fn.filereadable(result) ~= 0
 
 		if isReadable then
-			return testDir .. testFileName
+			return result
 		end
 	end
 
@@ -39,9 +40,8 @@ local function getProductionCodeFileJS()
 	local testFileName = vim.fn.expand("%:t")
 	local ft = vim.fn.expand("%:e")
 	local testFileDir = vim.fn.expand("%:h")
-	local endFile = "%.spec%." .. ft .. "$"
-
 	local fileDir = testFileDir:gsub("__tests__", "")
+	local endFile = "%.spec%." .. ft .. "$"
 
 	for _, ex in ipairs(JS_EXTENSIONS) do
 		local extension = "%." .. ex
