37fc5c1d88dc9247f516d3ecdc4717335792f4b6
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-05-17 20:08:39 -0400

    add rainbow brackets

---
+++ nvim/home/.config/nvim/lua/zsb/plugins/rainbow-delimeters-nvim.lua
@@ -0,0 +1,29 @@
+return {
+	"HiPhish/rainbow-delimiters.nvim",
+	config = Config("rainbow-delimiters.setup", function(rainbow)
+		local rainbow_delimiters = require("rainbow-delimiters")
+
+		rainbow.setup({
+			strategy = {
+				[""] = rainbow_delimiters.strategy["global"],
+				vim = rainbow_delimiters.strategy["local"],
+			},
+			query = {
+				[""] = "rainbow-delimiters",
+				lua = "rainbow-blocks",
+			},
+			priority = {
+				[""] = 110,
+				lua = 210,
+			},
+			highlight = {
+				"RainbowDelimiterYellow",
+				"RainbowDelimiterBlue",
+				"RainbowDelimiterOrange",
+				"RainbowDelimiterGreen",
+				"RainbowDelimiterViolet",
+				"RainbowDelimiterCyan",
+			},
+		})
+	end),
+}
