08c1a91fcff75a107c9da48db209af1b1037efd3
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-05-23 11:41:34 -0400

    put telescope things together

--- nvim/home/.config/nvim/lua/zsb/plugins/init.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/init.lua
@@ -8,11 +8,6 @@ return {
 		"windwp/nvim-ts-autotag",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 	},
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
-		keys = { "<leader>", "<C-p>" },
-	},
 	{ "moll/vim-bbye" },
 	-- if it doesn't work, follow https://github.com/ekickx/clipboard-image.nvim/issues/50
 	{ "ekickx/clipboard-image.nvim", ft = { "markdown" } },
--- nvim/home/.config/nvim/lua/zsb/plugins/telescope.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/telescope.lua
@@ -1,28 +1,36 @@
+local LAZY_KEYS = { "<leader>", "<C-p>" }
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
-	keys = { "<leader>", "<C-p>" },
-	config = Config("telescope", function(telescope)
-		telescope.setup({
-			defaults = {
-				path_display = { "truncate" },
-			},
-			extensions = {
-				fzf = {
-					fuzzy = true,
-					override_generic_sorter = true,
-					override_file_sorter = true,
-					case_mode = "smart_case",
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
+		keys = LAZY_KEYS,
+		config = Config("telescope", function(telescope)
+			telescope.setup({
+				defaults = {
+					path_display = { "truncate" },
 				},
-			},
-		})
-
-		kset("n", "<C-p>", function()
-			require("telescope.builtin").git_files({
-				show_untracked = true,
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
+				},
 			})
-		end)
 
-		telescope.load_extension("fzf")
-	end),
+			kset("n", "<C-p>", function()
+				require("telescope.builtin").git_files({
+					show_untracked = true,
+				})
+			end)
+
+			telescope.load_extension("fzf")
+		end),
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		keys = LAZY_KEYS,
+		build = "make",
+	},
 }
