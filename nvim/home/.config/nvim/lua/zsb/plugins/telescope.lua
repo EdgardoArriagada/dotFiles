e43cbeb88755a68548f92eefa833e65f51be1dbe
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-05-23 12:24:05 -0400

    improve telescope lazy load

--- nvim/home/.config/nvim/lua/zsb/plugins/telescope.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/telescope.lua
@@ -1,11 +1,9 @@
-local LAZY_KEYS = { "<C-p>" }
-local LAZY_CMD = "Telescope"
 return {
 	{
 		"nvim-telescope/telescope.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
-		keys = LAZY_KEYS,
-		cmd = LAZY_CMD,
+		keys = { "<C-p>" },
+		cmd = "Telescope",
 		config = Config("telescope", function(telescope)
 			telescope.setup({
 				defaults = {
@@ -32,8 +30,7 @@ return {
 	},
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
-		keys = LAZY_KEYS,
-		cmd = LAZY_CMD,
+		lazy = true,
 		build = "make",
 	},
 }
