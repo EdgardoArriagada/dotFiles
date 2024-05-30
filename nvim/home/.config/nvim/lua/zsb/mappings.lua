6492bcbe1f0f46ca1979e5826ea2d1f162e8a8a5
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-05-30 13:05:23 -0400

    use kset instead for mapping leader

--- nvim/home/.config/nvim/lua/zsb/mappings.lua
+++ nvim/home/.config/nvim/lua/zsb/mappings.lua
@@ -1,5 +1,5 @@
 --Remap space as leader key
-vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
+kset("", "<Space>", "<Nop>", { noremap = true, silent = true })
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
