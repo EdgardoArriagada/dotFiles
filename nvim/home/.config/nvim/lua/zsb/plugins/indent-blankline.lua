6093328402fd8b34ed836428a5ff503d4672ac6a
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-06-08 12:39:12 -0400

    format indent blankline

--- nvim/home/.config/nvim/lua/zsb/plugins/indent-blankline.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/indent-blankline.lua
@@ -1,19 +1,19 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  config = Config("ibl", function(ibl)
-    local hooks = require("ibl.hooks")
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	config = Config("ibl", function(ibl)
+		local hooks = require("ibl.hooks")
 
-    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-    end)
+		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+		end)
 
-    ibl.setup({
-      indent = { char = "│" },
-      whitespace = {
-        remove_blankline_trail = false,
-      },
-      scope = { enabled = true, highlight = "RainbowViolet" },
-    })
-  end),
+		ibl.setup({
+			indent = { char = "│" },
+			whitespace = {
+				remove_blankline_trail = false,
+			},
+			scope = { enabled = true, highlight = "RainbowViolet" },
+		})
+	end),
 }
