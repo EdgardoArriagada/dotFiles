4070610ad05ff45790be7b7da393bfc12ab08c27
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-06-16 17:09:16 -0400

    tweak delimeters

--- nvim/home/.config/nvim/lua/zsb/plugins/theme.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/theme.lua
@@ -4,12 +4,18 @@ return {
 	priority = 1000,
 	config = function()
 		local nordic = require("nordic")
+		-- https://github.com/AlexvZyl/nordic.nvim/blob/main/lua/nordic/colors/nordic.lua
 		local palette = require("nordic.colors")
 
 		nordic.setup({
+			-- https://neovim.io/doc/user/syntax.html#group-name
 			override = {
 				Visual = {
 					bg = palette.gray3,
+					bold = false,
+				},
+				Delimiter = {
+					fg = palette.white0_normal,
 					bold = false,
 				},
 			},
