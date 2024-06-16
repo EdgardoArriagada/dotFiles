68110d1254ee3be598086880125248840900bc4a
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-06-16 17:01:05 -0400

    improve visual highlight

--- nvim/home/.config/nvim/lua/zsb/plugins/theme.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/theme.lua
@@ -3,6 +3,18 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("nordic").load()
+		local nordic = require("nordic")
+		local palette = require("nordic.colors")
+
+		nordic.setup({
+			override = {
+				Visual = {
+					bg = palette.gray3,
+					bold = false,
+				},
+			},
+		})
+
+		nordic.load()
 	end,
 }
