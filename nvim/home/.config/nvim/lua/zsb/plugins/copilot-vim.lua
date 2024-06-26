4b2ccf4e9458acc9f6abe67fa60884d411fe23a2
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-06-25 23:30:07 -0400

    boy scout

--- nvim/home/.config/nvim/lua/zsb/plugins/copilot-vim.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/copilot-vim.lua
@@ -5,7 +5,7 @@ return {
 		vim.g.copilot_no_tab_map = true
 		vim.g.copilot_filetypes = { TelescopePrompt = false, harpoon = false }
 
-		kset("i", "<C-l>", 'copilot#Accept("")', { expr = true, silent = true, replace_keycodes = false })
+		kset("i", "<C-l>", "copilot#Accept()", { expr = true, silent = true, replace_keycodes = false })
 		kset("i", "<C-_>", "copilot#Dismiss()", { expr = true, silent = true })
 		kset("n", "<C-_>", "copilot#Dismiss()", { expr = true, silent = true })
 	end,
