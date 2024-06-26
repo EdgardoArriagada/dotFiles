f3cda7a3e87ee522bd1eaebfeb73166b47f5af0a
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-06-25 23:28:15 -0400

    use lua for mapping copilot instead

--- nvim/home/.config/nvim/lua/zsb/plugins/copilot-vim.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/copilot-vim.lua
@@ -3,9 +3,10 @@ return {
 	"github/copilot.vim",
 	config = function()
 		vim.g.copilot_no_tab_map = true
-		vim.cmd([[imap <silent><script><expr> <C-l> copilot#Accept("\<CR>")]])
-		vim.cmd([[imap <silent><script><expr> <C-_> copilot#Dismiss()]])
-		vim.cmd([[nmap <silent><script><expr> <C-_> copilot#Dismiss()]])
 		vim.g.copilot_filetypes = { TelescopePrompt = false, harpoon = false }
+
+		kset("i", "<C-l>", 'copilot#Accept("")', { expr = true, silent = true, replace_keycodes = false })
+		kset("i", "<C-_>", "copilot#Dismiss()", { expr = true, silent = true })
+		kset("n", "<C-_>", "copilot#Dismiss()", { expr = true, silent = true })
 	end,
 }
