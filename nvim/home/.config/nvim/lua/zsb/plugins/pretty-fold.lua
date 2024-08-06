440d300a314e88b3f42ba5dcddc2ab7122f671bd
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-08-06 13:00:17 -0400

    apply pretty-fold temp fix

--- nvim/home/.config/nvim/lua/zsb/plugins/pretty-fold.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/pretty-fold.lua
@@ -6,36 +6,38 @@
 --
 --
 return {
-  "anuvyklack/pretty-fold.nvim",
-  ft = { "json" },
-  config = Config("pretty-fold", function(prettyFold)
-    local calculateSpaces = function(foldLevel)
-      if foldLevel <= 2 then
-        return foldLevel
-      end
-      return (foldLevel * 2) - 2
-    end
+	"bbjornstad/pretty-fold.nvim", -- temporal solution https://github.com/anuvyklack/pretty-fold.nvim/pull/41#issuecomment-2161471200
+	-- use this after bbjornstad fork removed
+	--[[ "anuvyklack/pretty-fold.nvim", ]]
+	ft = { "json" },
+	config = Config("pretty-fold", function(prettyFold)
+		local calculateSpaces = function(foldLevel)
+			if foldLevel <= 2 then
+				return foldLevel
+			end
+			return (foldLevel * 2) - 2
+		end
 
-    prettyFold.setup({
-      keep_indentation = false,
-      fill_char = "━",
-      sections = {
-        left = {
-          "",
-          function()
-            return string.rep("·", calculateSpaces(vim.v.foldlevel) - 2)
-          end,
-          "content",
-          "┣",
-        },
-        right = {
-          "┫ ",
-          "number_of_folded_lines",
-          ": ",
-          "percentage",
-          " ┣━━",
-        },
-      },
-    })
-  end),
+		prettyFold.setup({
+			keep_indentation = false,
+			fill_char = "━",
+			sections = {
+				left = {
+					"",
+					function()
+						return string.rep("·", calculateSpaces(vim.v.foldlevel) - 2)
+					end,
+					"content",
+					"┣",
+				},
+				right = {
+					"┫ ",
+					"number_of_folded_lines",
+					": ",
+					"percentage",
+					" ┣━━",
+				},
+			},
+		})
+	end),
 }
