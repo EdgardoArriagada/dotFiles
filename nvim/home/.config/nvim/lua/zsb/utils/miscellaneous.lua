43ed59809685a783db85976824b9a1c631ac3fcd
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-08-26 10:53:32 -0400

    apply a small refactor

--- nvim/home/.config/nvim/lua/zsb/utils/miscellaneous.lua
+++ nvim/home/.config/nvim/lua/zsb/utils/miscellaneous.lua
@@ -126,16 +126,15 @@ end
 --- @return nil
 --- @param args OpenFileInPositionArgs
 function OpenFileInPosition(args)
-	local filename = args.filename
 	local current_filename = vim.fn.expand("%:p")
-	local normalized_filename = vim.fn.fnamemodify(filename, ":p")
+	local normalized_filename = vim.fn.fnamemodify(args.filename, ":p")
 
 	if current_filename == normalized_filename then
 		-- Add to jumplist
 		Exec("norm! m'")
 	else
 		-- Edit that file
-		Exec("e " .. filename)
+		Exec("e " .. args.filename)
 	end
 
 	vim.api.nvim_win_set_cursor(0, { args.lnum, args.col - 1 })
