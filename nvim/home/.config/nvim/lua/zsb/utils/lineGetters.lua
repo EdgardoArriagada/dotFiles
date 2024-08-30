f83bfaeb05a88611fd32fdc09ad67b9da2afefe3
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-08-30 19:33:00 -0400

    lint a file

--- nvim/home/.config/nvim/lua/zsb/utils/lineGetters.lua
+++ nvim/home/.config/nvim/lua/zsb/utils/lineGetters.lua
@@ -1,65 +1,67 @@
 function getSameIndentLine(direction, lineMarker)
-  local inc, endOfFile = GetDirectionalProps(direction)
-  local existsSameIndent = false
-  local originalIndent = indent('.')
+	local inc, endOfFile = GetDirectionalProps(direction)
+	local existsSameIndent = false
+	local originalIndent = indent(".")
 
-  while not existsSameIndent and lineMarker ~= endOfFile do
-    lineMarker = lineMarker + inc
-    existsSameIndent = indent(lineMarker) == originalIndent
+	while not existsSameIndent and lineMarker ~= endOfFile do
+		lineMarker = lineMarker + inc
+		existsSameIndent = indent(lineMarker) == originalIndent
 
-    if existsSameIndent and IsEmptyLine(lineMarker) then
-      existsSameIndent = false
-    end
-  end
+		if existsSameIndent and IsEmptyLine(lineMarker) then
+			existsSameIndent = false
+		end
+	end
 
-  if existsSameIndent then
-    return lineMarker
-  else
-    return line('.')
-  end
+	if existsSameIndent then
+		return lineMarker
+	else
+		return line(".")
+	end
 end
 
 function getLesserIndent(direction, lineMarker)
-  local inc, endOfFile = GetDirectionalProps(direction)
+	local inc, endOfFile = GetDirectionalProps(direction)
 
-  local originalIndent = indent(lineMarker)
+	local originalIndent = indent(lineMarker)
 
-  local gotLesserIndent = false
+	local gotLesserIndent = false
 
-  while lineMarker ~= endOfFile do
-    lineMarker = lineMarker + inc
+	while lineMarker ~= endOfFile do
+		lineMarker = lineMarker + inc
 
-    gotLesserIndent = indent(lineMarker) < originalIndent
+		gotLesserIndent = indent(lineMarker) < originalIndent
 
-    if gotLesserIndent and not IsEmptyLine(lineMarker) then break end
-  end
+		if gotLesserIndent and not IsEmptyLine(lineMarker) then
+			break
+		end
+	end
 
-  return lineMarker
+	return lineMarker
 end
 
 function getStopLine(direction)
-  local inc, endOfFile = GetDirectionalProps(direction)
+	local inc, endOfFile = GetDirectionalProps(direction)
 
-  local lineMarker = line('.')
-  local isStartEmpty = IsEmptyLine(lineMarker)
-  local originalIndent = indent('.')
+	local lineMarker = line(".")
+	local isStartEmpty = IsEmptyLine(lineMarker)
+	local originalIndent = indent(".")
 
-  local function didSwitch(line)
-     if isStartEmpty then
-       return not IsEmptyLine(line)
-     else
-       return IsEmptyLine(line)
-     end
-  end
+	local function didSwitch(line)
+		if isStartEmpty then
+			return not IsEmptyLine(line)
+		else
+			return IsEmptyLine(line)
+		end
+	end
 
-  while not didSwitch(lineMarker) and lineMarker ~= endOfFile do
-     lineMarker = lineMarker + inc
+	while not didSwitch(lineMarker) and lineMarker ~= endOfFile do
+		lineMarker = lineMarker + inc
 
-     if not IsEmptyLine(lineMarker) and indent(lineMarker) < originalIndent then
-       lineMarker = lineMarker - inc
-       break
-    end
-  end
+		if not IsEmptyLine(lineMarker) and indent(lineMarker) < originalIndent then
+			lineMarker = lineMarker - inc
+			break
+		end
+	end
 
-  return lineMarker
+	return lineMarker
 end
