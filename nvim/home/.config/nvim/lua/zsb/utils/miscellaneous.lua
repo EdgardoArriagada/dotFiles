f545ffcc350dc45da07fb6e7e35d5dd1651f050c
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-09-07 10:54:29 -0400

    move util to misc

--- nvim/home/.config/nvim/lua/zsb/utils/miscellaneous.lua
+++ nvim/home/.config/nvim/lua/zsb/utils/miscellaneous.lua
@@ -141,3 +141,26 @@ function OpenFileInPosition(args)
 
 	vim.api.nvim_win_set_cursor(0, { args.lnum, args.col - 1 })
 end
+
+function ShowMenu(conf, opts)
+	local height = conf.height or 20
+	local width = conf.width or 60
+	local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+
+	local Win_id = require("plenary.popup").create(opts or {}, {
+		title = conf.title,
+		line = math.floor(((vim.o.lines - height) / 2) - 1),
+		col = math.floor((vim.o.columns - width) / 2),
+		minwidth = width,
+		minheight = height,
+		borderchars = borderchars,
+		callback = conf.callback,
+	})
+
+	function CloseMenu()
+		vim.api.nvim_win_close(Win_id, true)
+	end
+
+	local bufnr = vim.api.nvim_win_get_buf(Win_id)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<cmd>lua CloseMenu()<CR>", { silent = false })
+end
