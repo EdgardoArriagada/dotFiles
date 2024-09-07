d7f5affcfe69d65ee6e049f1a9fbba0c2f0b8fa3
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-09-06 21:18:13 -0400

    replace copilot by the lua version

--- nvim/home/.config/nvim/lua/zsb/plugins/copilot-vim.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/copilot-vim.lua
@@ -1,12 +1,27 @@
 return {
 	event = "InsertEnter",
-	"github/copilot.vim",
+	cmd = "Copilot",
+	"zbirenbaum/copilot.lua",
 	config = function()
-		vim.g.copilot_no_tab_map = true
-		vim.g.copilot_filetypes = { TelescopePrompt = false, harpoon = false }
-
-		kset("i", "<C-l>", "copilot#Accept()", { expr = true, silent = true, replace_keycodes = false })
-		kset("i", "<C-_>", "copilot#Dismiss()", { expr = true, silent = true })
-		kset("n", "<C-_>", "copilot#Dismiss()", { expr = true, silent = true })
+		require("copilot").setup({
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				hide_during_completion = true,
+				debounce = 75,
+				keymap = {
+					accept = "<C-l>",
+					accept_word = false,
+					accept_line = false,
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-_>",
+				},
+			},
+			filetypes = {
+				TelescopePrompt = false,
+				harpoon = false,
+			},
+		})
 	end,
 }
