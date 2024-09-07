4debdb9ba2d2d1df82cbe368836170f87958ec32
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-09-07 11:46:21 -0400

    trim pasted in files

--- nvim/home/.config/nvim/lua/zsb/customPluggins/qf.lua
+++ nvim/home/.config/nvim/lua/zsb/customPluggins/qf.lua
@@ -15,7 +15,10 @@ function PasteToQf()
 			local file_list = vim.api.nvim_buf_get_lines(buf_id, 0, -1, false)
 
 			local qf_entries = {}
-			for _, file in ipairs(file_list) do
+			for _, rawFile in ipairs(file_list) do
+				-- trim leading and trailing whitespace
+				local file = rawFile:gsub("^%s*(.-)%s*$", "%1")
+
 				if file ~= "" then
 					table.insert(qf_entries, { filename = file, lnum = 1, col = 1 })
 				end
