37d13fedcebe68a69b7d450893ce28502079015a
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-09-13 21:20:24 -0300

    apply a small tweak

--- nvim/home/.config/nvim/lua/zsb/utils/miscellaneous.lua
+++ nvim/home/.config/nvim/lua/zsb/utils/miscellaneous.lua
@@ -147,7 +147,7 @@ function ShowMenu(conf, opts)
 	local width = conf.width or 60
 	local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
 
-	local Win_id = require("plenary.popup").create(opts or {}, {
+	local winId = require("plenary.popup").create(opts or {}, {
 		title = conf.title,
 		line = math.floor(((vim.o.lines - height) / 2) - 1),
 		col = math.floor((vim.o.columns - width) / 2),
@@ -158,9 +158,9 @@ function ShowMenu(conf, opts)
 	})
 
 	function CloseMenu()
-		vim.api.nvim_win_close(Win_id, true)
+		vim.api.nvim_win_close(winId, true)
 	end
 
-	local bufnr = vim.api.nvim_win_get_buf(Win_id)
+	local bufnr = vim.api.nvim_win_get_buf(winId)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<cmd>lua CloseMenu()<CR>", { silent = false })
 end
