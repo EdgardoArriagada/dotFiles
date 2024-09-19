67608c343394e6edf64e7b34c10fb2abcf77a59e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-09-18 21:49:40 -0300

    boy scout

--- nvim/home/.config/nvim/lua/zsb/neovide-config.lua
+++ nvim/home/.config/nvim/lua/zsb/neovide-config.lua
@@ -1,10 +1,16 @@
+local global = vim.g
+local set_keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
 vim.o.guifont = "Hack Nerd Font:h21"
-vim.g.neovide_scroll_animation_length = 0.6
-vim.g.neovide_fullscreen = true
+
+global.neovide_scroll_animation_length = 0.6
+global.neovide_fullscreen = true
 
 -- Allow clipboard copy paste in neovim
-vim.g.neovide_input_use_logo = 1
-vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+global.neovide_input_use_logo = 1
+
+set_keymap("", "<D-v>", "+p<CR>", opts)
+set_keymap("!", "<D-v>", "<C-R>+", opts)
+set_keymap("t", "<D-v>", "<C-R>+", opts)
+set_keymap("v", "<D-v>", "<C-R>+", opts)
