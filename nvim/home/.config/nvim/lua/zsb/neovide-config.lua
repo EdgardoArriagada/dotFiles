d93ab6a0834f951acb1ff9e2dd29e8a0d6c39f17
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-09-18 22:20:09 -0300

    remove telescope scroll

--- nvim/home/.config/nvim/lua/zsb/neovide-config.lua
+++ nvim/home/.config/nvim/lua/zsb/neovide-config.lua
@@ -2,9 +2,23 @@ local global = vim.g
 local set_keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-global.neovide_scroll_animation_length = 0.6
-global.neovide_fullscreen = false
+local autocmd = vim.api.nvim_create_autocmd
+local group = vim.api.nvim_create_augroup("TelescopePreview", { clear = true })
 
+autocmd("FileType", {
+	pattern = "TelescopePreview",
+	callback = function()
+		vim.g.neovide_scroll_animation_length = 0
+	end,
+	group = group,
+})
+autocmd("BufLeave", {
+	pattern = "*",
+	callback = function()
+		vim.g.neovide_scroll_animation_length = 0.6
+	end,
+	group = group,
+})
 -- Allow clipboard copy paste in neovim
 global.neovide_input_use_logo = 1
 
