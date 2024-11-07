0b0332c41635b8412f1e8394198eaa17a4fa649a
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-11-07 10:23:19 -0300

    fix a bug when opening popups

--- nvim/home/.config/nvim/lua/zsb/autocmd/rename-tmux-tab.lua
+++ nvim/home/.config/nvim/lua/zsb/autocmd/rename-tmux-tab.lua
@@ -1,13 +1,22 @@
 Cautocmd("VimEnter", {
 	callback = function()
+		if vim.g.zsb_prevent_renametab == 1 then
+			return
+		end
+
 		SetTimeout(function()
 			vim.fn.system('tmux rename-window "  $(get_repo_name)"')
-		end, 1000)
+		end, 600)
 	end,
+	group = Group,
 })
 
 Cautocmd("VimLeave", {
 	callback = function()
+		if vim.g.zsb_prevent_renametab == 1 then
+			return
+		end
+
 		vim.fn.system("tmux set automatic-rename")
 	end,
 })
