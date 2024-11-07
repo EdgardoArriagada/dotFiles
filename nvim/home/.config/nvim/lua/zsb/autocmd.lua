86ee4fa4f3178ba7c1f0c6c62d865c05722d044d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-11-07 00:03:04 -0300

    impove styling

--- nvim/home/.config/nvim/lua/zsb/autocmd.lua
+++ nvim/home/.config/nvim/lua/zsb/autocmd.lua
@@ -51,7 +51,7 @@ Cautocmd("FileChangedShellPost", {
 Cautocmd("VimEnter", {
 	callback = function()
 		SetTimeout(function()
-			vim.fn.system('tmux rename-window " $(get_repo_name)"')
+			vim.fn.system('tmux rename-window "  $(get_repo_name)"')
 		end, 0)
 	end,
 })
