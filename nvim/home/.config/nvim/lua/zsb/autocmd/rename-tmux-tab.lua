fb2c7a875b9a99a8467893fb259b90f33ed90f60
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-11-09 11:59:08 -0300

    use vim system instead

--- nvim/home/.config/nvim/lua/zsb/autocmd/rename-tmux-tab.lua
+++ nvim/home/.config/nvim/lua/zsb/autocmd/rename-tmux-tab.lua
@@ -4,11 +4,11 @@ Cautocmd("VimEnter", {
 			return
 		end
 
-		local win_id = vim.fn.system("tmux display-message -p '#{window_id}'"):gsub("%s+", "")
+		local win_id = vim.system({ "tmux", "display-message", "-p", "#{window_id}" }):wait().stdout:gsub("%s+", "")
 
 		SetTimeout(function()
 			local currentFile = vim.fn.expand("%:p")
-			vim.fn.system("zsb_charm_tmux_renametab " .. win_id .. " " .. currentFile)
+			vim.system({ "zsb_charm_tmux_renametab", win_id, currentFile })
 		end, 600)
 	end,
 	group = Group,
@@ -20,6 +20,6 @@ Cautocmd("VimLeave", {
 			return
 		end
 
-		vim.fn.system("tmux set automatic-rename")
+		vim.system({ "tmux", "set", "automatic-rename" })
 	end,
 })
