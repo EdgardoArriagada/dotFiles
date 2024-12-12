3c52b25683837dd3b89524d473a69bc4252bba75
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-12-12 17:48:53 -0300

    improve view

--- nvim/home/.config/nvim/lua/zsb/plugins/csvview-nvim.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/csvview-nvim.lua
@@ -2,7 +2,11 @@ return {
 	"hat0uma/csvview.nvim",
 	ft = "csv",
 	config = Config("csvview", function(plugin)
-		plugin.setup()
+		plugin.setup({
+			view = {
+				display_mode = "border",
+			},
+		})
 		plugin.enable()
 	end),
 }
