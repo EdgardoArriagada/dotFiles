17fd01e46f78a5c893a123427caad21786d0a57d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-12-23 12:02:38 -0300

    prevent copilot twice

--- nvim/home/.config/nvim/lua/zsb/plugins/avante-nvim.lua
+++ nvim/home/.config/nvim/lua/zsb/plugins/avante-nvim.lua
@@ -15,10 +15,43 @@ return {
 		--- The below dependencies are optional,
 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 		{
+			event = "InsertEnter",
+			cmd = "Copilot",
 			"zbirenbaum/copilot.lua",
-			config = Config("copilot", function(plugin)
-				plugin.setup()
-			end),
+			config = function()
+				require("copilot").setup({
+					suggestion = {
+						enabled = true,
+						auto_trigger = true,
+						hide_during_completion = true,
+						debounce = 75,
+						keymap = {
+							accept = "<C-l>",
+							accept_word = false,
+							accept_line = false,
+							next = "<M-]>",
+							prev = "<M-[>",
+							dismiss = "<C-_>",
+						},
+					},
+					filetypes = {
+						TelescopePrompt = false,
+						harpoon = false,
+						NvimTree = false,
+						dbui = false,
+						qf = false,
+						hgcommit = false,
+						gitrebase = false,
+						gitcommit = false,
+						help = false,
+						alpha = false,
+						oil = false,
+						["."] = false,
+					},
+				})
+
+				Kset("n", "<C-_>", require("copilot.suggestion").dismiss)
+			end,
 		}, -- for providers='copilot'
 		{
 			-- support for image pasting
