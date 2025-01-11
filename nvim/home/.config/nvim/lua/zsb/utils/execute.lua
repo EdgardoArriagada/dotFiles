98af7575e309c054d5f80005b17b692b6bf9537d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-01-10 21:55:17 -0300

    replace for nvim_exec2

--- nvim/home/.config/nvim/lua/zsb/utils/execute.lua
+++ nvim/home/.config/nvim/lua/zsb/utils/execute.lua
@@ -1,8 +1,8 @@
 function Exec(str)
-	vim.api.nvim_exec(str, false)
+	vim.api.nvim_exec2(str, {})
 end
 
 -- Use this when Exec has characters like <CR> or <C-...>
 function SafeExec(str)
-	vim.api.nvim_exec(vim.api.nvim_replace_termcodes(str, true, true, true), false)
+	vim.api.nvim_exec2(vim.api.nvim_replace_termcodes(str, true, true, true), {})
 end
