02619b6eb0e5a8bb6954807af3faf5b3d66799f8
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-04-21 16:18:02 -0400

    migrate roman expansion to nvim

--- nvim/home/.config/nvim/lua/zsb/commands.lua
+++ nvim/home/.config/nvim/lua/zsb/commands.lua
@@ -1,6 +1,12 @@
 local createCmd = vim.api.nvim_create_user_command
 
 createCmd("W", function()
+	-- Create the directory if it doesn't exist
+	local file_path = vim.fn.expand("%:p")
+	if vim.fn.filereadable(file_path) == 0 then
+		vim.fn.mkdir(vim.fn.fnamemodify(file_path, ":h"), "p")
+	end
+
 	Exec("w!")
 	Exec("e!")
 end, {})
