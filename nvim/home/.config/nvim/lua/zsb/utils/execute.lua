53599d21f965cc4d4bcccbd2d61a117847c10f6c
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-04-24 14:20:18 -0400

    improve docs

--- nvim/home/.config/nvim/lua/zsb/utils/execute.lua
+++ nvim/home/.config/nvim/lua/zsb/utils/execute.lua
@@ -1,8 +1,11 @@
+-- Alias to vim.cmd
+-- When using characters like <CR> or <C-...> use SafeExec instead
 function Exec(str)
 	vim.api.nvim_exec2(str, {})
 end
 
--- Use this when Exec has characters like <CR> or <C-...>
+-- Alias to vim.cmd
+-- If not using characters like <CR> or <C-...> use Exec instead
 function SafeExec(str)
 	vim.api.nvim_exec2(vim.api.nvim_replace_termcodes(str, true, true, true), {})
 end
