5b362dbd471b5b9b27ccd5543359303b022493a6
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-05-23 14:08:21 -0400

    replace neovide with cursor

--- hammerspoon/home/.hammerspoon/init.lua
+++ hammerspoon/home/.hammerspoon/init.lua
@@ -1,5 +1,5 @@
 -- for this script to work:
--- remember to enable "Accessibility" permissions to Hammerspoon 
+-- remember to enable "Accessibility" permissions to Hammerspoon
 -- in the "Security & Privacy" panel in OSX System Preferences.
 
 local u = require("utils")
@@ -14,6 +14,6 @@ hs.hotkey.bind(cmd2, "1", u.weakFocus("Slack"))
 hs.hotkey.bind(cmd2, "2", u.focusApp("Google Chrome"))
 hs.hotkey.bind(cmd2, "3", u.weakFocus("IntelliJ IDEA"))
 hs.hotkey.bind(cmd2, "4", u.weakFocus("Postman"))
-hs.hotkey.bind(cmd2, "5", u.weakFocus("Neovide"))
+hs.hotkey.bind(cmd2, "5", u.weakFocus("Cursor"))
 hs.hotkey.bind(cmd2, "9", u.focusApp("Spotify"))
 hs.hotkey.bind(cmd2, "h", u.hideAllApps)
