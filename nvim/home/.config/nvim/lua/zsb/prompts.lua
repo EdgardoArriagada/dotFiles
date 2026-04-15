806d550ce5144f9bebbaad76d9ae966f87206d6f
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2026-04-15 16:31:28 -0400

    improve prompt

--- nvim/home/.config/nvim/lua/zsb/prompts.lua
+++ nvim/home/.config/nvim/lua/zsb/prompts.lua
@@ -3,9 +3,10 @@ local M = {}
 M.debug = {
 	desc = "Debug",
 	get = function()
-		return "- add debug logs writing to the filesystem any information you think is neccessary\n"
-			.. "- ask me to run the script again and I will tell you when is done so you can check the logs\n"
-			.. "- lets repeat untill we find the issue"
+		return [[
+- add debug logs writing to the filesystem any information you think is neccessary
+- ask me to run the script again and I will tell you when is done so you can check the logs
+- lets repeat untill we find the issue]]
 	end,
 }
 
