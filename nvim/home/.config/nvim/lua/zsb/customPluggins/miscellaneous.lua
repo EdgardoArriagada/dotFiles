9fff9c323f3641aa5e367e4e91c6ca397d56b3c9
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2026-07-23 12:53:32 -0400

    improve toggler

--- nvim/home/.config/nvim/lua/zsb/customPluggins/miscellaneous.lua
+++ nvim/home/.config/nvim/lua/zsb/customPluggins/miscellaneous.lua
@@ -39,28 +39,72 @@ function ToggleRelativeNumber()
 end
 
 function ToggleBool()
-	local word = vim.fn.expand("<cword>")
 	local map = {
 		["true"] = "false", ["false"] = "true",
 		["True"] = "False", ["False"] = "True",
 		["TRUE"] = "FALSE", ["FALSE"] = "TRUE",
 	}
-	local replacement = map[word]
-	if not replacement then return end
 
 	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
 	local line = vim.api.nvim_get_current_line()
 	local cursor_lua = col + 1
-	local search_from = 1
 
-	while true do
-		local s, e = line:find(word, search_from, true)
-		if not s then return end
-		if s <= cursor_lua and cursor_lua <= e then
-			vim.api.nvim_buf_set_text(0, row - 1, s - 1, row - 1, e, { replacement })
-			return
+	local function is_word(ln, s, e)
+		return (s == 1 or not ln:sub(s - 1, s - 1):match("%w"))
+			and (e == #ln or not ln:sub(e + 1, e + 1):match("%w"))
+	end
+
+	local function apply(r, s, w)
+		vim.api.nvim_buf_set_text(0, r - 1, s - 1, r - 1, s - 1 + #w, { map[w] })
+	end
+
+	-- cursor on a boolean: toggle in place
+	for w in pairs(map) do
+		local s = 1
+		while true do
+			s = line:find(w, s, true)
+			if not s then break end
+			local e = s + #w - 1
+			if is_word(line, s, e) and s <= cursor_lua and cursor_lua <= e then
+				apply(row, s, w)
+				return
+			end
+			s = e + 1
 		end
-		search_from = e + 1
+	end
+
+	-- scan forward line by line
+	for r = row, vim.api.nvim_buf_line_count(0) do
+		local ln = vim.api.nvim_buf_get_lines(0, r - 1, r, false)[1]
+		local from = r == row and cursor_lua + 1 or 1
+		local best_s, best_w = nil, nil
+		for w in pairs(map) do
+			local s = ln:find(w, from, true)
+			if s and is_word(ln, s, s + #w - 1) and (not best_s or s < best_s) then
+				best_s, best_w = s, w
+			end
+		end
+		if best_s then apply(r, best_s, best_w) return end
+	end
+
+	-- nothing forward: scan backward line by line
+	for r = row, 1, -1 do
+		local ln = vim.api.nvim_buf_get_lines(0, r - 1, r, false)[1]
+		local upto = r == row and cursor_lua - 1 or #ln
+		local best_s, best_w = nil, nil
+		for w in pairs(map) do
+			local s = 1
+			while true do
+				s = ln:find(w, s, true)
+				if not s then break end
+				local e = s + #w - 1
+				if e <= upto and is_word(ln, s, e) and (not best_s or s > best_s) then
+					best_s, best_w = s, w
+				end
+				s = e + 1
+			end
+		end
+		if best_s then apply(r, best_s, best_w) return end
 	end
 end
 
